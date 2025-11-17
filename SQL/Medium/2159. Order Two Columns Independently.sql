aed03a24c92594f48ebae7f56bc4a12724dabbe6
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-17 14:14:35 +0100

    Create 2159. Order Two Columns Independently.sql

---
+++ SQL/Medium/2159. Order Two Columns Independently.sql
@@ -0,0 +1,17 @@
+
+
+         -- Approach 1. Using two - CTE with Window Function - ROW_NUMBER() -- 
+WITH 
+	f_col AS (
+      SELECT first_col, 
+      	ROW_NUMBER() OVER(ORDER BY first_col) rn 
+      FROM data
+	), 
+    s_col AS (
+      SELECT second_col, 
+      	ROW_NUMBER() OVER(ORDER BY second_col DESC) rn
+      FROM DATA
+)
+SELECT first_col, second_col 
+FROM f_col 
+JOIN s_col USING (rn);
