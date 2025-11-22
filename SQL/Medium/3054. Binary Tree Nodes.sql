a10b07c8eca4fa210fbf4adc2d9ca736f78b2b0b
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-22 14:49:34 +0100

    Create 3054. Binary Tree Nodes.sql

---
+++ SQL/Medium/3054. Binary Tree Nodes.sql
@@ -0,0 +1,12 @@
+
+
+         -- Approach 1. Using - CASE...WHEN conditions -- 
+SELECT n N,
+       CASE
+           WHEN NOT EXISTS (SELECT 1 FROM tree t2 WHERE t2.p = t1.n) THEN 'Leaf'
+           WHEN p IS NULL THEN 'Root'
+           ELSE 'Inner'
+       END Type
+FROM tree t1
+ORDER BY n;
+
