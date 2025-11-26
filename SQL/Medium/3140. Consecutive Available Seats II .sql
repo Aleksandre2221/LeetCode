5f56924e94441a79e1a20f487c9f2e66550ef6e5
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-11-26 12:55:53 +0100

    Create 3140. Consecutive Available Seats II .sql

---
+++ SQL/Medium/3140. Consecutive Available Seats II .sql
@@ -0,0 +1,22 @@
+
+
+           -- Approach 1. Using two - CTE -- 
+WITH 
+  cons_groups AS (
+    SELECT *, 
+      seat_id - ROW_NUMBER() OVER(ORDER BY seat_id) grp_id
+    FROM cinema
+    WHERE free = TRUE
+  ), 
+  groups_cnt AS (
+    SELECT grp_id,
+      MIN(seat_id) first_seat_id,
+      MAX(seat_id) last_seat_id,
+      COUNT(*) consecutive_seats_len, 
+      RANK() OVER(ORDER BY COUNT(*) DESC) rnk 
+    FROM cons_groups
+    GROUP BY grp_id
+)
+SELECT first_seat_id, last_seat_id, consecutive_seats_len
+FROM groups_cnt
+WHERE rnk = 1;
