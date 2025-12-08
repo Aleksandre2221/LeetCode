dcd06451de17caa6261310177205a02a7c2c0a1e
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-08 13:28:37 +0100

    Create 3475. DNA Pattern Recognition .sql

---
+++ SQL/Medium/3475. DNA Pattern Recognition .sql
@@ -0,0 +1,9 @@
+
+
+         -- Approach 1. Using multiple - CASE...WHEN conditions -- 
+SELECT *,
+  CASE WHEN LEFT(dna_sequence, 3) = 'ATG' THEN 1 ELSE 0 END has_start,
+  CASE WHEN RIGHT(dna_sequence, 3) IN ('TAA', 'TAG', 'TGA') THEN 1 ELSE 0 END has_stop,
+  CASE WHEN dna_sequence LIKE '%ATAT%' THEN 1 ELSE 0 END has_atat,
+  CASE WHEN dna_sequence LIKE '%GGG%' THEN 1 ELSE 0 END has_ggg
+FROM samples;
