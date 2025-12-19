7be7598dce5f8c6ae4fa885ec2a5df2172a1531a
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-19 16:32:10 +0100

    Implement monthly transactions calculation with CTEs
    Added two approaches to calculate monthly chargebacks and approved transactions using CTEs.

--- SQL/Medium/1205. Monthly Transactions II.sql
+++ SQL/Medium/1205. Monthly Transactions II.sql
@@ -1,5 +1,8 @@
 
 
+-- Risolved: 2 times 
+
+
          -- Approach 1. Using two - CTE and - LEFT JOIN --  
 WITH 
 	chargebacks_monthly  AS (
@@ -30,3 +33,32 @@ SELECT cm.month, cm.country,
 FROM chargebacks_monthly cm 
 LEFT JOIN approved_monthly am ON cm.country = am.country AND cm.month = am.month
 ORDER BY cm.month;
+
+
+
+			 -- Approach 2. Using tow - CTE -- 
+WITH 
+	cb AS (
+        SELECT TO_CHAR(c.trans_date, 'YYYY-MM') "month", t.country,
+          COUNT(c.trans_id) chargeback_count, 
+          SUM(t.amount) chargeback_amount
+        FROM chargebacks c 
+        LEFT JOIN transactions t ON t.id = c.trans_id
+        GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
+	),
+    approved AS (
+    	SELECT TO_CHAR(c.trans_date, 'YYYY-MM') "month", t.country,
+			SUM(CASE WHEN t.state = 'approved' THEN 1 ELSE 0 END) approved_count,
+    		SUM(CASE WHEN t.state = 'approved' THEN t.amount ELSE 0 END) approved_amount
+		FROM chargebacks c 
+		LEFT JOIN transactions t ON TO_CHAR(c.trans_date, 'YYYY-MM') = TO_CHAR(t.trans_date, 'YYYY-MM')
+		GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
+)
+SELECT cb.month, cb.country, a.approved_count, a.approved_amount, cb.chargeback_count, cb.chargeback_amount
+FROM cb  
+JOIN approved a USING (month)
+
+
+
+
+
