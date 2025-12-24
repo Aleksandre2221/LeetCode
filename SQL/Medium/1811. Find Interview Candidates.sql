3590df692b8ac0c5367f29eb966cb4e8a27dcf12
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-24 15:56:34 +0100

    Update 1811. Find Interview Candidates.sql

--- SQL/Medium/1811. Find Interview Candidates.sql
+++ SQL/Medium/1811. Find Interview Candidates.sql
@@ -3,7 +3,7 @@
 -- Risolved: 2 times 
 
 
-         -- Approach 1. Using multiple - CTE -- 
+         -- Approach 1. Using advanced technique - "Gaps and Islands" -- 
 WITH 
     unpivot AS (
       SELECT contest_id, gold_medal user_id FROM contests
@@ -35,6 +35,32 @@ WHERE user_id IN (SELECT user_id FROM consecutive_cnt)
 	OR user_id IN (SELECT user_id FROM gold_medals);
 
 
+
+
+				 -- Approach 2. Using - CTE with Window Functions - LAG() / LEAD() - less performant --  
+WITH 
+	all_medals AS (
+      SELECT contest_id, gold_medal user_id FROM contests 
+      UNION ALL 
+      SELECT contest_id, silver_medal FROM contests 
+      UNION ALL  
+      SELECT contest_id, bronze_medal 
+      FROM contests
+  	),
+    grps AS (
+      SELECT 
+      	CASE  
+      		WHEN contest_id - LAG(contest_id) OVER(PARTITION BY user_id ORDER BY contest_id) = 1
+      			AND contest_id - LAG(contest_id, 2) OVER(PARTITION BY user_id ORDER BY contest_id) = 2
+      		THEN user_id  
+      	END user_id
+      FROM all_medals
+)
+SELECT name, mail  
+FROM users 
+WHERE user_id IN (SELECT gold_medal FROM contests GROUP BY gold_medal HAVING COUNT(*) > 2) 
+	OR user_id IN (SELECT user_id FROM grps);
+                  
 
 
 
