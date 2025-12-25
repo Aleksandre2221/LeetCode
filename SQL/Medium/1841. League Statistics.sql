5bc40e839d0368ea54478302bec63a719389f230
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-25 10:07:38 +0100

    Update 1841. League Statistics.sql

--- SQL/Medium/1841. League Statistics.sql
+++ SQL/Medium/1841. League Statistics.sql
@@ -1,5 +1,8 @@
 
 
+-- Risolved: 2 times
+
+
          -- Approach 1. Using - UNION ALL -- 
 WITH all_teams AS (
 	SELECT home_team_id AS team_id, away_team_id, home_team_goals, away_team_goals
@@ -11,11 +14,12 @@ WITH all_teams AS (
 SELECT t.team_name,	
 	COUNT(*) matches_played,
 	SUM(
-      CASE 
-      		WHEN home_team_goals>away_team_goals THEN 3 
-  			  WHEN home_team_goals=away_team_goals THEN 1
-  			  ELSE 0
-      END) points,
+		CASE 
+			WHEN home_team_goals>away_team_goals THEN 3 
+			WHEN home_team_goals=away_team_goals THEN 1
+			ELSE 0
+     	END
+	) points,
 	SUM(home_team_goals) goal_for,
 	SUM(away_team_goals) goal_against,  
 	SUM(home_team_goals) - SUM(away_team_goals) goal_diff
@@ -27,7 +31,49 @@ ORDER BY points DESC, goal_diff DESC, team_name;
 
 
 
-         -- Approach 2. Using multiple - CTE, without - UNION ALL -- 
+		 -- Approach 2. Usomg two - CTE -- 
+WITH 
+	home_teams AS (
+      SELECT home_team_id, 
+      	SUM(home_team_goals) goals_for, 
+      	SUM(away_team_goals) goals_against, 
+      	COUNT(*) matches_played,
+      	SUM(
+          	CASE 	
+          		WHEN away_team_goals < home_team_goals THEN 3 
+          		WHEN away_team_goals = home_team_goals THEN 1 ELSE 0 
+          	END) pts
+      FROM matches  
+      GROUP BY home_team_id
+	), 
+    away_teams AS (
+	  SELECT away_team_id, 
+      	SUM(away_team_goals) goals_for, 
+      	SUM(home_team_goals) goals_against, 
+      	COUNT(*) matches_played,
+      	SUM(
+          	CASE 	
+          		WHEN away_team_goals > home_team_goals THEN 3 
+          		WHEN away_team_goals = home_team_goals THEN 1 ELSE 0 
+          	END) pts
+      FROM matches  
+      GROUP BY away_team_id
+)
+SELECT t.team_name, 
+	COALESCE(ht.matches_played + at.matches_played, 0) matches_played, 
+	COALESCE(ht.pts + at.pts) points,
+    COALESCE(ht.goals_for + at.goals_for, 0) goal_for, 
+    COALESCE(ht.goals_against + at.goals_against, 0) goal_against,
+    COALESCE((ht.goals_for + at.goals_for) - (ht.goals_against + at.goals_against), 0) goal_diff
+FROM teams t
+LEFT JOIN home_teams ht ON ht.home_team_id = t.team_id
+LEFT JOIN away_teams at ON at.away_team_id = t.team_id
+ORDER BY points DESC, goal_diff DESC, team_name;
+
+
+
+
+         -- Approach 3. Using multiple - CTE --   
 WITH 
   points AS (
     SELECT *,
@@ -71,3 +117,7 @@ FROM teams t
 LEFT JOIN home_stats h ON t.team_id = h.team_id
 LEFT JOIN away_stats a ON t.team_id = a.team_id
 ORDER BY points DESC, goal_diff DESC, team_name;
+
+
+
+
