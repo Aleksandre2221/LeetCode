8e7b77d58521dbd5d7afdc374c3e964cb46e4e38
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-26 10:30:52 +0100

    Update 2142. The Number of Passengers in Each Bus I.sql

--- SQL/Medium/2142. The Number of Passengers in Each Bus I.sql
+++ SQL/Medium/2142. The Number of Passengers in Each Bus I.sql
@@ -1,7 +1,25 @@
 
 
-         -- Approach 1. Using - CTE -- 
-WITH passenger_bus AS (
+-- Risolved: 2 times
+
+
+          -- Approach 1. Using - CTE with a Window Function - ROW_NUMBER() -- 
+WITH assigned_bus AS (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY p.passenger_id ORDER BY b.arrival_time) AS rn
+    FROM passengers p
+    JOIN buses b ON p.arrival_time <= b.arrival_time
+)
+SELECT b.bus_id, COUNT(ab.passenger_id) AS passenger_count
+FROM buses b
+LEFT JOIN assigned_bus ab ON b.bus_id = ab.bus_id AND ab.rn = 1
+GROUP BY b.bus_id
+ORDER BY b.bus_id;
+
+
+
+         -- Approach 2. Using - CTE -- 
+WITH assigned_bus AS (
     SELECT p.passenger_id,
         MIN(b.bus_id) bus_id
     FROM passengers p
@@ -9,7 +27,27 @@ WITH passenger_bus AS (
     GROUP BY p.passenger_id
 )
 SELECT b.bus_id,
-    COUNT(pb.passenger_id) passenger_count
+    COUNT(ab.passenger_id) passenger_count
 FROM buses b
-LEFT JOIN passenger_bus pb ON b.bus_id = pb.bus_id
-GROUP BY b.bus_id;
+LEFT JOIN assigned_bus ab ON b.bus_id = ab.bus_id
+GROUP BY b.bus_id
+ORDER BY b.bus_id;
+
+
+
+         -- Approach 3. Using - CTE with - DISTINCT ON condition - (Postgres ONLY) -- 
+WITH assigned_bus AS (
+  SELECT DISTINCT ON (p.passenger_id) * 
+  FROM buses b 
+  JOIN passengers p ON P.arrival_time <= b.arrival_time
+)
+SELECT b.bus_id, COUNT(ab.passenger_id)  
+FROM buses b 
+LEFT JOIN assigned_bus ab ON b.bus_id = ab.bus_id
+GROUP BY b.bus_id
+ORDER BY b.bus_id;
+
+
+
+
+
