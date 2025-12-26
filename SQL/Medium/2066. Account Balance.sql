391143e77f07d014f5c8a137aceeb6fdace1978e
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2025-12-26 10:17:21 +0100

    Update 2066. Account Balance.sql

--- SQL/Medium/2066. Account Balance.sql
+++ SQL/Medium/2066. Account Balance.sql
@@ -1,5 +1,8 @@
 
 
+-- Risolved: 2 times
+
+
          -- The Best Approach. Using - CASE...WHEN condition within Window Function - SUM --  
 SELECT account_id, day,  
 	SUM(CASE WHEN type = 'Deposit' THEN amount ELSE -amount END) OVER(PARTITION BY account_id ORDER BY DAy) balance
