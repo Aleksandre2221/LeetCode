b3dbb1d29ba8efa189bfe38cca7010c9b4850568
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-10 18:50:26 +0100

    Refactor SQL for finding friends without mutual friends
    Refactored SQL query to find friends with no mutual friends using two approaches: CTE with NOT IN and NOT EXISTS.

--- SQL/Medium/3058. Friends With No Mutual Friends.sql
+++ SQL/Medium/3058. Friends With No Mutual Friends.sql
@@ -1,10 +1,13 @@
 
 
+-- Risolved: 2 times 
+
+
          -- Approach 1. Using - CTE and - WHERE NOT IN condition -- 
-WITH all_combos AS (
-	SELECT user_id1, user_id2 FROM Friends
+WITH two_way_friends AS (
+	SELECT user_id1, user_id2 FROM friends
 	UNION ALL
-	SELECT user_id2, user_id1 FROM Friends
+	SELECT user_id2, user_id1 FROM friends
 )
 SELECT user_id1, user_id2
 FROM friends
@@ -14,3 +17,27 @@ WHERE (user_id1, user_id2) NOT IN (
 	JOIN all_combos ac2 ON ac1.user_id2 = ac2.user_id2
 )
 ORDER BY user_id1, user_id2;
+
+
+		-- Approach 2. Using - WHERE NOT EXISTS condition -- 
+WITH two_way_friends AS (
+	SELECT user_id1 user_id, user_id2 friend FROM friends 
+	UNION 
+	SELECT user_id2, user_id1 FROM friends
+)
+SELECT f.user_id1, f.user_id2 
+FROM friends f
+WHERE NOT EXISTS (
+	SELECT 1 
+	FROM two_way_friends twf1 
+	JOIN two_way_friends twf2 USING (friend)
+	WHERE twf1.user_id = f.user_id1 AND twf2.user_id = f.user_id2
+)
+ORDER BY user_id1, user_id2;
+	
+
+
+	
+
+
+
