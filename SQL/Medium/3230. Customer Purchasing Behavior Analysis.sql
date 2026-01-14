b03cf87a4946f3a07187d6b820778861de64de4d
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-14 18:16:41 +0100

    Implement CTE for customer purchasing analysis
    Added a second approach using a Common Table Expression (CTE) to analyze customer purchasing behavior.

--- SQL/Medium/3230. Customer Purchasing Behavior Analysis.sql
+++ SQL/Medium/3230. Customer Purchasing Behavior Analysis.sql
@@ -28,3 +28,33 @@ JOIN products p ON p.product_id = t.product_id
 JOIN category_ranking cr ON cr.customer_id = t.customer_id AND cr.rnk = 1
 GROUP BY t.customer_id
 ORDER BY loyalty_score DESC, customer_id;
+
+
+
+          -- Approach 2. Using one - CTE -- 
+WITH category_info AS (
+  SELECT t.customer_id, p.category, 
+           DENSE_RANK() OVER(
+                           PARTITION BY t.customer_id 
+                           ORDER BY COUNT(*) DESC, MAX(t.transaction_date) DESC) rnk
+  FROM transactions t  
+  JOIN products p ON t.product_id = p.product_id
+  GROUP BY t.customer_id, p.category
+)
+SELECT t.customer_id, 
+    SUM(t.amount) total_amount, 
+    COUNT(t.transaction_id) transaction_cnt, 
+    COUNT(DISTINCT p.category) category_cnt,
+    MAX(ci.category) most_frequent_category,
+    ROUND(AVG(t.amount), 2) avg_amount, 
+    ROUND((COUNT(t.transaction_id) * 10.0) + (sum(t.amount) / 100.0), 2) loyalty_score
+FROM transactions t 
+JOIN products p USING (product_id)
+JOIN category_info ci USING (customer_id)
+WHERE ci.rnk = 1 
+GROUP BY t.customer_id
+ORDER BY loyalty_score DESC, customer_id;
+
+
+
+
