e0e5967026d6c53b2d6ba4afb45151b265cbca4f
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-17 11:33:22 +0100

    Implement CTE approaches for overbooked employees
    Added two approaches for finding overbooked employees using CTEs.

--- SQL/Medium/3611. Find Overbooked Employees.sql
+++ SQL/Medium/3611. Find Overbooked Employees.sql
@@ -1,5 +1,8 @@
 
 
+-- Risolved: 2 times
+
+
          -- Approach 1. Using - CTE with - DATE_TRUNC -- 
 WITH hours_cnt AS (
   SELECT e.employee_id, e.employee_name, e.department, 
@@ -16,3 +19,23 @@ FROM hours_cnt
 GROUP BY employee_id, employee_name, department
 HAVING COUNT(week) > 1 
 ORDER BY meeting_heavy_weeks DESC, employee_name;
+
+
+
+          -- Approach 2. Using - CTE with - TO_CHAR -- 
+WITH valid_empl AS (
+  SELECT e.employee_id, e.employee_name, e.department, 
+      TO_CHAR(m.meeting_date, 'IW')
+  FROM meetings m
+  JOIN employees e USING (employee_id)
+  GROUP BY e.employee_id, e.employee_name, e.department, TO_CHAR(m.meeting_date, 'IW') 
+  HAVING SUM(m.duration_hours) > 20.0 
+)
+SELECT employee_id, employee_name, department, 
+   COUNT(*) meeting_heavy_weeks  
+FROM valid_empl 
+GROUP BY employee_id, employee_name, department 
+HAVING COUNT(*) >= 2
+ORDER BY meeting_heavy_weeks DESC, employee_name;
+
+
