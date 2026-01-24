1b83e4e7329de15fc5c83a8aceff46eac337adfe
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-24 09:39:39 +0100

    Update 2308. Arrange Table by Gender.sql

--- SQL/Medium/2308. Arrange Table by Gender.sql
+++ SQL/Medium/2308. Arrange Table by Gender.sql
@@ -1,19 +1,19 @@
 
 
--- Rosolved: 2 times
+-- Rosolved: 3 times
 
 
          -- Approach 1. Using - CTE with a Window Function - ROW_NUMBER() and - CASE...WHEN conditions -- 
 WITH row_num AS (
-  SELECT *,
-    ROW_NUMBER() OVER(PARTITION BY gender ORDER BY user_id) rn1, 
-    CASE
-       WHEN gender = 'female' THEN 1
-       WHEN gender = 'other' THEN 2	
-       ELSE 3
-   END rn2
-FROM genders
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY gender ORDER BY user_id) rn1, 
+        CASE
+            WHEN gender = 'female' THEN 1
+            WHEN gender = 'other' THEN 2	
+            ELSE 3
+       END rn2
+    FROM genders
+    ORDER BY rn1, rn2
 )
 SELECT user_id, gender 
-FROM row_num 
-ORDER BY rn1, rn2;
+FROM row_num;
