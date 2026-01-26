6601c1773e1ce4026fa08f0cf3e57ac09c66c494
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-26 10:54:23 +0100

    Refactor SQL queries for continuous ranges

--- SQL/Medium/1285. Find the Start and End Number of Continuous Ranges.sql
+++ SQL/Medium/1285. Find the Start and End Number of Continuous Ranges.sql
@@ -1,14 +1,20 @@
 
 
+-- Risolved: 2 times 
+
+
         -- Approach 1. Using - CTE with Window Function - ROW_NUMBER() -- 
 WITH row_num AS (
   SELECT log_id, 
-  	log_id - ROW_NUMBER() OVER(ORDER BY log_id) rn
+        log_id - ROW_NUMBER() OVER(ORDER BY log_id) rn
   FROM log 
 )
-SELECT MIN(log_id) start_date, MAX(log_id) end_date
+SELECT 
+    MIN(log_id) start_date, 
+    MAX(log_id) end_date
 FROM row_num
-GROUP BY rn 
+GROUP BY rn
+ORDER BY start_date;
 
 
 
@@ -21,11 +27,13 @@ WITH t1 AS (
             CASE
                 WHEN (log_id - LAG(log_id) OVER (ORDER BY log_id)) = 1 THEN 0
                 ELSE 1
-            END AS start_date
+            END start_date
         FROM log
     ) sub
 ) 
-SELECT MIN(log_id) AS start_id, MAX(log_id) AS end_id
+SELECT 
+    MIN(log_id) start_id, 
+    MAX(log_id) end_id
 FROM t1
 GROUP BY pid
 ORDER BY start_id;
