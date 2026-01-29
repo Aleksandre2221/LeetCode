6f527c02a11a06b6e09608b34b7e9c27016fe921
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-01-29 09:25:08 +0100

    Refactor SQL query for strong friendship problem
    Refactored SQL query to use CTE for better readability and efficiency.

--- SQL/Medium/1949. Strong Friendship.sql
+++ SQL/Medium/1949. Strong Friendship.sql
@@ -1,16 +1,22 @@
 
 
--- Risolved: 2 times
+-- Risolved: 3 times
 
 
          -- Approach 1. Using - CTE with - UNION ALL - 
-WITH all_friends AS (
-  SELECT * FROM friendship 
-  UNION ALL  
-  SELECT user2_id, user1_id FROM friendship
+WITH 
+    bidir AS (
+        SELECT user1_id, user2_id FROM friendship 
+        UNION ALL 
+        SELECT user2_id, user1_id FROM friendship
+    ), 
+    com_fr AS ( 
+      SELECT b1.user1_id, b2.user2_id, COUNT(*) common_friend
+      FROM bidir b1  
+      JOIN bidir b2 ON b1.user2_id = b2.user1_id AND b1.user1_id < b2.user2_id
+      GROUP BY b1.user1_id, b2.user2_id
+      HAVING COUNT(*) >= 3
 )
-SELECT a1.user1_id, a2.user1_id user2_id, COUNT(*) common_friend
-FROM all_friends a1
-JOIN all_friends a2 ON a1.user1_id < a2.user1_id AND a1.user2_id = a2.user2_id
-GROUP BY a1.user1_id, a2.user1_id
-HAVING COUNT(*) > 2;
+SELECT * 
+FROM com_fr 
+WHERE (user1_id, user2_id) IN (SELECT * FROM friendship);
