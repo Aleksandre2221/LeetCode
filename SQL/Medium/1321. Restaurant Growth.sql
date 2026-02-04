48f2f23408536a59287f8a71963d235d3e21ff61
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-04 14:47:32 +0100

    Update 1321. Restaurant Growth.sql

--- SQL/Medium/1321. Restaurant Growth.sql
+++ SQL/Medium/1321. Restaurant Growth.sql
@@ -1,5 +1,6 @@
 
--- Risolved: 3 times
+
+-- Risolved: 4 times
 
 
          -- Approach 1. Using - CTE and Window Functions - SUM(), - AVG() with - ROWS BETWEEN condition -- 
@@ -11,26 +12,30 @@ WITH day_sum AS (
 )
 SELECT visited_on, 
 	SUM(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) amount, 
-	ROUND(
-      		AVG(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
-      	, 2) average_amount 
+	ROUND(AVG(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) average_amount 
 FROM day_sum 
 OFFSET 6;
 
 
          -- Approach 2. Without using Window Funciotns -- 
-WITH valid_dates AS (
-  SELECT DISTINCT visited_on 
-  FROM customer 
-  ORDER BY visited_on 
-  OFFSET 6
+WITH 
+    daily_amount AS (
+        SELECT visited_on, SUM(amount) amount
+        FROM customer
+        GROUP BY visited_on
+    ), 
+    valid_dates AS (
+        SELECT visited_on
+        FROM daily_amount
+        WHERE visited_on - INTERVAL '6 days' >= (SELECT MIN(visited_on) FROM customer)
 )
 SELECT vd.visited_on, 
-	SUM(c.amount) amount,
-    ROUND(AVG(c.amount)::numeric, 2) average_amount
-FROM customer c 
-JOIN valid_dates vd ON c.visited_on BETWEEN vd.visited_on - INTERVAL '6 days' AND vd.visited_on 
-GROUP BY vd.visited_on;
+    SUM(da.amount) amount, 
+    ROUND(AVG(da.amount), 2) average_amount
+FROM daily_amount da 
+JOIN valid_dates vd ON da.visited_on BETWEEN vd.visited_on - INTERVAL'6 days' AND vd.visited_on 
+GROUP BY vd.visited_on
+ORDER BY visited_on;
 
 
 		 -- Approach 3. Without using - OFFSET -- 
