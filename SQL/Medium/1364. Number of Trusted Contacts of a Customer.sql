1a4dc1fe717c0c0028f9861a214d44324a5a55a2
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-05 15:11:07 +0100

    Update 1364. Number of Trusted Contacts of a Customer.sql

--- SQL/Medium/1364. Number of Trusted Contacts of a Customer.sql
+++ SQL/Medium/1364. Number of Trusted Contacts of a Customer.sql
@@ -1,6 +1,9 @@
 
 
-         -- The Best Approach. Using two - JOIN and - SUM with - CASE..WHEN condition --
+-- Risolved: 2 times 
+
+
+         -- Approach 1. Using two - JOIN and - SUM with - CASE..WHEN condition --
 SELECT i.invoice_id, c.customer_name, i.price,
     COUNT(co.contact_email) AS contacts_cnt,
     SUM(CASE WHEN co.contact_email IN (SELECT email FROM customers) THEN 1 ELSE 0 END) trusted_contacts_cnt
@@ -10,3 +13,14 @@ LEFT JOIN contacts co ON co.user_id = c.customer_id
 GROUP BY i.invoice_id, c.customer_name, i.price
 ORDER BY i.invoice_id;
 
+
+         -- Approach 2. Using two - LEFT JOIN -- 
+SELECT i.invoice_id, c.customer_name, i.price,
+    COUNT(ct.contact_email) contacts_cnt,
+    COUNT(cus2.customer_id) trusted_contacts_cnt
+FROM invoices i
+JOIN customers c ON i.user_id = c.customer_id
+LEFT JOIN contacts ct ON ct.user_id = c.customer_id
+LEFT JOIN customers cus2 ON ct.contact_email = cus2.email
+GROUP BY i.invoice_id, c.customer_name, i.price
+ORDER BY i.invoice_id;
