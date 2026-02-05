4c59757053e8816bbd1c4fa21ba626da6568ebfd
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-05 12:06:25 +0100

    Update 1205. Monthly Transactions II.sql

--- SQL/Medium/1205. Monthly Transactions II.sql
+++ SQL/Medium/1205. Monthly Transactions II.sql
@@ -3,65 +3,62 @@
 -- Risolved: 3 times 
 
 
-         -- Approach 1. Using two - CTE and - LEFT JOIN --  
+         -- Approach 1. Using two - CTE and - FULL JOIN --  
 WITH 
-	chargebacks_monthly  AS (
+	approved AS ( 
       SELECT 
-          TO_CHAR(c.trans_date, 'YYYY-MM') "month",
-		  t.country,
-          SUM(t.amount) chargeback_amount, 
-          COUNT(*) chargeback_count
-      FROM transactions t
-      JOIN chargebacks c ON t.id = c.trans_id
-      GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
-    ),
-	approved_monthly  AS (
-      SELECT 
-          TO_CHAR(trans_date, 'YYYY-MM') "month",
-          country,
-          COUNT(*) FILTER (WHERE state = 'approved') approved_count,
-          SUM(amount) FILTER (WHERE state = 'approved') approved_amount
-      FROM transactions
+      		TO_CHAR(trans_date, 'YYYY-MM') "month", 
+      		country,
+      		SUM(amount) amount,
+      		COUNT(id) cnt
+      FROM transactions 
+      WHERE state = 'approved' 
       GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
+	), 
+    charged_b AS (
+      SELECT 
+      		TO_CHAR(c.trans_date, 'YYYY-MM') "month", 
+      		t.country,
+			SUM(t.amount) amount,
+      		COUNT(c.trans_id) cnt 
+      FROM chargebacks c  
+      JOIN transactions t ON c.trans_id = t.id 
+      GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
 )
-SELECT cm.month, cm.country, 
-    COALESCE(am.approved_count, 0) approved_count,
-    COALESCE(am.approved_amount, 0) approved_amount,
-    COALESCE(cm.chargeback_count, 0) chargeback_count,
-    COALESCE(cm.chargeback_amount, 0) chargeback_amount
-FROM chargebacks_monthly cm 
-LEFT JOIN approved_monthly am ON cm.country = am.country AND cm.month = am.month
-ORDER BY cm.month;
+SELECT 
+	COALESCE(a.month, cb.month) "month", 
+    COALESCE(a.country, cb.country) country, 
+    COALESCE(a.cnt, 0) approved_count, 
+    COALESCE(a.amount, 0) approved_amount, 
+    COALESCE(cb.cnt, 0) chargeback_count, 
+    COALESCE(cb.amount, 0) chargeback_amount
+FROM approved a 
+FULL JOIN charged_b cb ON a.month = cb.month AND a.country = cb.country;
+WHERE COALESCE(a.amount, 0) <> 0
+	OR COALESCE(cb.amount, 0) <> 0;
 
 
 
-			 -- Approach 2. Using tow - CTE -- 
-WITH 
-	cb AS (
-		SELECT 
-			TO_CHAR(c.trans_date, 'YYYY-MM') "month", 
-			t.country,
-			COUNT(c.trans_id) chargeback_count, 
-          	SUM(t.amount) chargeback_amount
-        FROM chargebacks c 
-        LEFT JOIN transactions t ON t.id = c.trans_id
-        GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
-	),
-    approved AS (
-    	SELECT 
-			TO_CHAR(c.trans_date, 'YYYY-MM') "month", 
-			t.country,
-			SUM(CASE WHEN t.state = 'approved' THEN 1 ELSE 0 END) approved_count,
-    		SUM(CASE WHEN t.state = 'approved' THEN t.amount ELSE 0 END) approved_amount
-		FROM chargebacks c 
-		LEFT JOIN transactions t ON TO_CHAR(c.trans_date, 'YYYY-MM') = TO_CHAR(t.trans_date, 'YYYY-MM')
-		GROUP BY TO_CHAR(c.trans_date, 'YYYY-MM'), t.country
+			-- Approach 2. Using - UNION ALL -- 
+WITH all_events AS (
+  SELECT id, country, state, amount, trans_date FROM transactions
+  UNION ALL  
+  SELECT t.id, t.country, 'chargeback' state, t.amount, c.trans_date 
+  FROM transactions t  
+  JOIN chargebacks c ON t.id = c.trans_id
 )
-SELECT cb.month, cb.country, a.approved_count, a.approved_amount, cb.chargeback_count, cb.chargeback_amount
-FROM cb  
-JOIN approved a USING (month)
-ORDER BY cb.month;
-
+SELECT 
+	TO_CHAR(trans_date, 'YYYY-MM') "month", 
+    country, 
+    SUM(CASE WHEN state = 'approved' THEN 1 else 0 END) approved_count,
+    SUM(CASE WHEN state = 'approved' THEN amount else 0 END) approved_amount,
+    SUM(CASE WHEN state = 'chargeback' THEN 1 else 0 END) chargeback_count,
+    SUM(CASE WHEN state = 'chargeback' THEN amount else 0 END) chargeback_amount
+FROM all_events
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
+HAVING 
+	SUM(CASE WHEN state = 'approved' THEN amount else 0 END) <> 0 
+    OR SUM(CASE WHEN state = 'chargeback' THEN amount else 0 END) <> 0;
 
 
 
