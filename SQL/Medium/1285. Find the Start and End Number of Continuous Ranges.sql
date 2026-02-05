30ca97b0993133f020b493225a7a9faf4072dec7
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-05 14:34:54 +0100

    Update 1285. Find the Start and End Number of Continuous Ranges.sql

--- SQL/Medium/1285. Find the Start and End Number of Continuous Ranges.sql
+++ SQL/Medium/1285. Find the Start and End Number of Continuous Ranges.sql
@@ -1,6 +1,6 @@
 
 
--- Risolved: 2 times 
+-- Risolved: 3 times 
 
 
         -- Approach 1. Using - CTE with Window Function - ROW_NUMBER() -- 
@@ -15,25 +15,3 @@ SELECT
 FROM row_num
 GROUP BY rn
 ORDER BY start_date;
-
-
-
-  
-         -- Approach 2. Using - Subquery within - CTE and Window Function - SUM() -- 
-WITH t1 AS (
-    SELECT log_id, SUM(start_date) OVER (ORDER BY log_id) pid
-    FROM (
-        SELECT log_id,
-            CASE
-                WHEN (log_id - LAG(log_id) OVER (ORDER BY log_id)) = 1 THEN 0
-                ELSE 1
-            END start_date
-        FROM log
-    ) sub
-) 
-SELECT 
-    MIN(log_id) start_id, 
-    MAX(log_id) end_id
-FROM t1
-GROUP BY pid
-ORDER BY start_id;
