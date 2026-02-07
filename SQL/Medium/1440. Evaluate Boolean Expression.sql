e54e98ea4d068802aae2a55569a338abf80fb461
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-07 11:34:09 +0100

    Update 1440. Evaluate Boolean Expression.sql

--- SQL/Medium/1440. Evaluate Boolean Expression.sql
+++ SQL/Medium/1440. Evaluate Boolean Expression.sql
@@ -3,7 +3,7 @@
 -- Risolved: 2 times 
 
 
-         -- The Best Approach. Using two - JOIN and - CASE...WHEN conditions -- 
+         -- Approach 1. Using two - JOIN and - CASE...WHEN conditions -- 
 SELECT e.*,
 	CASE  
 		WHEN e.operator = '=' AND lv.value <> rv.value THEN FALSE  
@@ -14,3 +14,21 @@ SELECT e.*,
 FROM expressions e 
 JOIN variables lv ON e.left_operand = lv.name
 JOIN variables rv ON e.right_operand = rv.name;
+
+
+
+		-- Approach 2. Compact version of CASE...WHEN -- 
+SELECT e.*,
+    CASE e.operator
+        WHEN '>' THEN v1.value > v2.value
+        WHEN '<' THEN v1.value < v2.value
+        WHEN '=' THEN v1.value = v2.value
+        ELSE TRUE
+    END value
+FROM expressions e
+JOIN variables v1 ON e.left_operand = v1.name
+JOIN variables v2 ON e.right_operand = v2.name;
+
+
+
+
