23f13c8e8fa4ec95b46f56e91ff9def7fa9cd8e0
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-09 10:45:10 +0100

    Update 1789. Primary Department for Each Employee.sql

--- SQL/Easy/1789. Primary Department for Each Employee.sql
+++ SQL/Easy/1789. Primary Department for Each Employee.sql
@@ -1,5 +1,8 @@
 
 
+-- Risolved: 3 times
+
+
          -- Approach 1. Using - LEFT JOIN and - COALESCE -- 
 SELECT DISTINCT e1.employee_id,
        COALESCE(e2.department_id, e1.department_id) department_id
@@ -13,8 +16,8 @@ LEFT JOIN employee e2 ON e1.employee_id = e2.employee_id AND e2.primary_flag = '
 SELECT DISTINCT e1.employee_id, 
 	CASE 
     	WHEN e2.department_id IS NOT NULL THEN e2.department_id
-      ELSE e1.department_id
-  END department_id
+      	ELSE e1.department_id
+    END department_id
 FROM employee e1 
 LEFT JOIN employee e2 ON e1.employee_id = e2.employee_id AND e2.primary_flag = 'Y';
 
@@ -28,6 +31,6 @@ WHERE primary_flag='Y'
 	OR employee_id IN (
         SELECT employee_id
         FROM Employee
-        Group by employee_id
-        having count(employee_id)=1
+        GROUP BY employee_id
+        HAVING COUNT(employee_id) = 1
 );
