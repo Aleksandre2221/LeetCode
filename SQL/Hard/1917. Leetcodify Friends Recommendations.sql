154e4e17c138acda7da351935d419c67227c0c0e
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-11 17:13:17 +0100

    Refactor SQL query to use CTE for friend recommendations
    Updated the SQL query to use CTE for recommendations.

---
+++ SQL/Hard/1917. Leetcodify Friends Recommendations.sql
@@ -0,0 +1,15 @@
+
+
+
+         -- Approach 1. Using - CTE -- 
+WITH valid_users AS (
+	SELECT l1.user_id, l2.user_id AS reccomended_id, l1.day
+  FROM listens l1  
+  JOIN listens l2 ON l1.user_id < l2.user_id AND l1.song_id = l2.song_id AND l1.day = l2.day
+  WHERE (l1.user_id, l2.user_id) NOT IN (SELECT user1_id, user2_id FROM friendship)
+  GROUP BY l1.user_id, l2.user_id, l1.day
+  HAVING COUNT(DISTINCT l1.song_id) >= 3
+)
+SELECT user_id, reccomended_id FROM valid_users 
+UNION ALL 
+SELECT reccomended_id, user_id FROM valid_users; 
