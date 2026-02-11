5f339bcbe90f8b9fd70477d3a4bea2328c241ad0
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-11 16:23:22 +0100

    Update 1949. Strong Friendship.sql

--- SQL/Medium/1949. Strong Friendship.sql
+++ SQL/Medium/1949. Strong Friendship.sql
@@ -1,6 +1,6 @@
 
 
--- Risolved: 3 times
+-- Risolved: 4 times
 
 
          -- Approach 1. Using - CTE with - UNION ALL - 
@@ -19,4 +19,27 @@ WITH
 )
 SELECT * 
 FROM com_fr 
-WHERE (user1_id, user2_id) IN (SELECT * FROM friendship);
+WHERE (user1_id, user2_id) IN (SELECT * FROM bidir);
+
+
+
+         -- Approach 2. Using two - JOIN -- 
+WITH bidir AS (
+  SELECT user1_id user, user2_id friend FROM friendship
+  UNION ALL
+  SELECT user2_id, user1_id FROM friendship
+)
+
+SELECT 
+    b1.user user1_id,
+    b2.user user2_id,
+    COUNT(*) common_friend
+FROM bidir b1
+JOIN bidir b2 
+    ON b1.friend = b2.friend
+    AND b1.user < b2.user
+JOIN friendship f
+    ON f.user1_id = b1.user 
+    AND f.user2_id = b2.user
+GROUP BY b1.user, b2.user
+HAVING COUNT(*) >= 3;
