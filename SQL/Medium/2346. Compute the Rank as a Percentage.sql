24a6c9dc72df349c49be49d4098f914dc3188904
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-12 10:53:53 +0100

    Update 2346. Compute the Rank as a Percentage.sql

--- SQL/Medium/2346. Compute the Rank as a Percentage.sql
+++ SQL/Medium/2346. Compute the Rank as a Percentage.sql
@@ -4,30 +4,26 @@
 SELECT student_id, department_id,
     COALESCE(
         ROUND(
-            	(rank() OVER (PARTITION BY department_id ORDER BY mark DESC) - 1) * 100 
-                  / (count(*) OVER (PARTITION BY department_id) - 1)
-            , 1
+            	(RANK() OVER (PARTITION BY department_id ORDER BY mark DESC) - 1) * 100 
+                  / (COUNT(*) OVER (PARTITION BY department_id) - 1)
+            , 2
         ), 0
     ) percentage
 FROM students;
 
 
 
-        -- Approach 2. Using two - CTE (more datiled) -- 
-WITH 
-  student_cnt AS (
-    SELECT department_id, COUNT(*) cnt
-    FROM students 
-    GROUP BY department_id
-  ), 
-  ranking AS (
-    SELECT *, RANK() OVER(PARTITION BY department_id ORDER BY mark DESC) rnk
-    FROM students 
+        -- Approach 2. Using - CTE with Window Functions -- (more datailed) -- 
+WITH ranking AS (
+  SELECT *, 
+      DENSE_RANK() OVER(PARTITION BY department_id ORDER BY mark DESC) rnk, 
+      COUNT(*) OVER(PARTITION BY department_id) cnt  
+  FROM students
 ) 
-SELECT r.student_id, r.department_id, 
-  ROUND((r.rnk - 1) * 100 / (sc.cnt - 1), 1) percentage 
-FROM ranking r
-JOIN student_cnt sc ON sc.department_id = r.department_id;
+SELECT student_id, department_id, 
+   MAX(ROUND((rnk - 1) * 100.0 / (cnt - 1), 2)) percentage
+FROM ranking 
+GROUP BY student_id, department_id;
 
 
 
