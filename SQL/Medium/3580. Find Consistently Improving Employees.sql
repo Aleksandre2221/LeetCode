3d4983adb3b0f59b43aebbdd62d13331b42002d0
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-12 10:06:32 +0100

    Update 3580. Find Consistently Improving Employees.sql

--- SQL/Medium/3580. Find Consistently Improving Employees.sql
+++ SQL/Medium/3580. Find Consistently Improving Employees.sql
@@ -3,32 +3,23 @@
 -- Risolved: 3 times
 
 
-         -- Approach 1. Using two - CTE -- 
-WITH 
-  row_num AS (
-    SELECT *, 
-        ROW_NUMBER() OVER(PARTITION BY employee_id ORDER BY review_date DESC) rn
-    FROM performance_reviews 
-  ),
-  rating_labels AS (
-    SELECT *, 
-      CASE  
-          WHEN rating > LEAD(rating) OVER(PARTITION BY employee_id ORDER BY review_date DESC)
-              AND rating < LAG(rating) OVER(PARTITION BY employee_id ORDER BY review_date DESC)
-          THEN 0 
-          ELSE 1 
-      END valid_rating
-    FROM row_num  
-    WHERE rn <= 3 
+		-- Approach 1. Using - CTE with - ROW_NUMBER() -- 
+WITH row_num AS (
+	SELECT *, 
+      ROW_NUMBER() OVER(PARTITION BY employee_id ORDER BY review_date DESC) rn 
+    FROM performance_reviews
 )
 SELECT e.employee_id, e.name, 
-  MAX(rl.rating) - MIN(rl.rating) improvement_score
-FROM rating_labels rl
-JOIN employees e ON e.employee_id = rl.employee_id
+	MAX(CASE WHEN rn.rn = 1 THEN rn.rating END) - MAX(CASE WHEN rn.rn = 3 THEN rn.rating END) improvement_score
+FROM row_num rn 
+JOIN employees e USING (employee_id)
+WHERE rn <= 3 
 GROUP BY e.employee_id, e.name
-HAVING COUNT(valid_rating) = 3 
-  AND SUM(valid_rating) = 2
+HAVING COUNT(rn.review_id) = 3 
+	AND MAX(CASE WHEN rn.rn = 1 THEN rn.rating END) > MAX(CASE WHEN rn.rn = 2 THEN rn.rating END)
+    AND MAX(CASE WHEN rn.rn = 2 THEN rn.rating END) > MAX(CASE WHEN rn.rn = 3 THEN rn.rating END)
 ORDER BY improvement_score DESC, name;
+
 
 
          -- Approach 2. Using - array and indexing (Only PostgreSQL) -- 
@@ -46,45 +37,6 @@ JOIN
 WHERE ratings[1] > ratings[2] AND ratings[2] > ratings[3]
 ORDER BY improvement_score DESC, name;
 
-
-
-		 -- Approach 3. Using one - CTE -- 
-WITH is_increasing AS (
-	SELECT pr.employee_id, pr.rating,
-		CASE  
-			WHEN rating - LEAD(rating) OVER(PARTITION BY employee_id ORDER BY review_date DESC) > 0 
-      		THEN 1 
-      		ELSE 0 
-      	END is_increasing,
-      	ROW_NUMBER() OVER(PARTITION BY pr.employee_id ORDER BY pr.review_date DESC) rn
-	FROM performance_reviews pr
-)
-SELECT e.employee_id, e.name, 
-	MAX(incr.rating) - MIN(incr.rating) improvement_score
-FROM is_increasing incr 
-JOIN employees e USING (employee_id)
-WHERE incr.rn <= 3 
-GROUP BY e.employee_id, e.name
-HAVING COUNT(*) > 2 
-	AND SUM(incr.is_increasing) >= 2 
-	AND MAX(incr.rating) - MIN(incr.rating) > 1
-ORDER BY improvement_score DESC, name;
-
-
-
-		 -- Approach 4. Using one - CTE with - ROW_NUMBER() -- 
-WITH rating_diff AS (
-    SELECT employee_id, rating,
-        ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY review_date DESC) AS rn
-    FROM performance_reviews 
-)
-SELECT e.employee_id, e.name,
-    MAX(CASE WHEN rd.rn = 1 THEN rd.rating END) - MAX(CASE WHEN rd.rn = 3 THEN rd.rating END) AS improvement_score
-FROM rating_diff rd
-JOIN employees e USING (employee_id)
-GROUP BY e.employee_id, e.name
-HAVING MAX(CASE WHEN rd.rn = 1 THEN rd.rating END) - MAX(CASE WHEN rd.rn = 3 THEN rd.rating END) >= 2
-ORDER BY improvement_score DESC, name;
 
 
 
