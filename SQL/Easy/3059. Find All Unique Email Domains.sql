d17a26ea4432c80b3a07fac25a52f8495e83993f
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-14 12:23:08 +0100

    Update 3059. Find All Unique Email Domains.sql

--- SQL/Easy/3059. Find All Unique Email Domains.sql
+++ SQL/Easy/3059. Find All Unique Email Domains.sql
@@ -1,10 +1,12 @@
 
 
--- Risolved: 2 times
+-- Risolved: 3 times
 
 
          -- Approach 1. Using - SPLIT_PART (Only in PostgreSQL) -- 
-SELECT SPLIT_PART(email, '@', 2) email_domain, COUNT(*) 
+SELECT 
+    SPLIT_PART(email, '@', 2) email_domain, 
+    COUNT(*) 
 FROM emails
 WHERE RIGHT(email, 4) = '.com'
 GROUP BY SPLIT_PART(email, '@', 2);
@@ -12,7 +14,9 @@ GROUP BY SPLIT_PART(email, '@', 2);
 
 
          -- Approach 2. Using - SUBTRING(... FROM POSITION (...)) -- SQL Standard
-SELECT SUBSTRING(email FROM POSITION('@' IN email) + 1), COUNT(*)
+SELECT 
+    SUBSTRING(email FROM POSITION('@' IN email) + 1), 
+    COUNT(*)
 FROM emails
 WHERE RIGHT(email, 4) = '.com'
 GROUP BY SUBSTRING(email FROM POSITION('@' IN email) + 1);
