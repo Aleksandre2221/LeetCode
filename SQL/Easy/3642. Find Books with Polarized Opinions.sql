fc4eba17d62a02d39258c0e3410b8a0b543f4d76
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-02-16 15:35:05 +0100

    Update 3642. Find Books with Polarized Opinions.sql

--- SQL/Easy/3642. Find Books with Polarized Opinions.sql
+++ SQL/Easy/3642. Find Books with Polarized Opinions.sql
@@ -1,11 +1,11 @@
 
 
--- Risolved: 2 times 
+-- Risolved: 3 times 
 
          -- Approach 1. Using multiple - HAVING conditions -- 
 SELECT b.book_id, b.title, b.author, b.genre, b.pages, 
     MAX(rs.session_rating) - MIN(rs.session_rating) rating_spread,
-    ROUND(SUM(CASE WHEN rs.session_rating >= 4 OR rs.session_rating <= 2 THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 2) polarization_score
+    ROUND(SUM(CASE WHEN rs.session_rating >= 4 OR rs.session_rating <= 2 THEN 1 ELSE 0 END) * 1.0 / COUNT(DISTINCT rs.session_id), 2) polarization_score
 FROM reading_sessions rs
 JOIN books b ON b.book_id = rs.book_id
 GROUP BY b.book_id, b.title, b.author, b.genre, b.pages
@@ -13,5 +13,5 @@ HAVING
     COUNT(rs.session_id) >= 5
     AND SUM(CASE WHEN rs.session_rating >= 4 THEN 1 ELSE 0 END) > 0
     AND SUM(CASE WHEN rs.session_rating <= 2 THEN 1 ELSE 0 END) > 0
-    AND ROUND(SUM(CASE WHEN rs.session_rating >= 4 OR rs.session_rating <= 2 THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 2) >= 0.6
+    AND ROUND(SUM(CASE WHEN rs.session_rating >= 4 OR rs.session_rating <= 2 THEN 1 ELSE 0 END) * 1.0 / COUNT(DISTINCT rs.session_id), 2) >= 0.6
 ORDER BY polarization_score DESC, title DESC;
