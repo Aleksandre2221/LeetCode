5b60af00f3046cfa42f5c393996bc072e8d68528
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-03 17:41:07 +0100

    Update 597. Friend Requests I -  Overall Acceptance Rate.sql

--- SQL/Easy/597. Friend Requests I -  Overall Acceptance Rate.sql
+++ SQL/Easy/597. Friend Requests I -  Overall Acceptance Rate.sql
@@ -3,8 +3,8 @@
          -- The Best Approach. Using - ROUND / NULLIF / Subquery --  
 SELECT 
 	ROUND(
-      	NULLIF(
-				(SELECT COUNT(DISTINCT (accepter_id, requester_id)) FROM request_accepted)::numeric 
-  	  			/ (SELECT COUNT(*) FROM friend_request)
+      	COALESCE(
+				(SELECT COUNT(DISTINCT (accepter_id, requester_id)) FROM RequestAccepted )::numeric 
+  	  			/ NULLIF((SELECT COUNT(DISTINCT (sender_id, send_to_id)) FROM FriendRequest), 0)
           	, 0)
   	, 2) accept_rate
