975b0c1992e016b6db0bb446a47bfd02ea56ca25
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-04 11:44:13 +0100

    Update 579. Find Cumulative Salary of an Employee.sql

--- SQL/Hard/579. Find Cumulative Salary of an Employee.sql
+++ SQL/Hard/579. Find Cumulative Salary of an Employee.sql
@@ -1,26 +1,13 @@
 
 
-         -- Approach 1. Using - CTE for Anti-Join -- 
-WITH most_recent AS (
-  SELECT id, max(month) "month" 
-  FROM employee 
-  GROUP BY id 
-  ORDER BY 1, 2
-)
-SELECT e.id, e.month, 
-	SUM(e.salary) OVER(PARTITION BY e.id ORDER BY e.month) salary 
-FROM employee e  
-JOIN most_recent mr ON e.id = mr.id AND e.month <> mr.month
-ORDER BY id, month DESC; 
-
-
-         -- Approach 2. Using - NOT IN condition -- 
-SELECT id, month, 
-  SUM(salary) OVER(PARTITION BY id ORDER BY month) salary
-FROM employee
-WHERE (id, month) NOT IN (
-  SELECT id, MAX(month)
-  FROM employee 
-  GROUP BY id
-)
-ORDER BY id, month DESC;
+         -- Approach 1. Using - CTE with Window Functions -- 
+WITH calculations AS (
+    SELECT id, month, 
+        SUM(salary) OVER(PARTITION BY id ORDER BY month RANGE 2 PRECEDING) salary,
+        RANK() OVER(PARTITION BY id ORDER BY month DESC) rnk
+    FROM employee
+ )
+ SELECT id, month, salary
+ FROM calculations 
+ WHERE rnk > 1
+ ORDER BY id, month DESC;
