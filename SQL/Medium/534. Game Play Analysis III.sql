abdd33830e32933fea1d48b6346241ed573e44ea
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-04 11:17:53 +0100

    Update 534. Game Play Analysis III.sql

--- SQL/Medium/534. Game Play Analysis III.sql
+++ SQL/Medium/534. Game Play Analysis III.sql
@@ -3,12 +3,12 @@
       -- Approach 1. Using Window Function - SUM -- 
 SELECT player_id, event_date, 
 	SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) games_played_so_far
-FROM playeractivity
+FROM activity
 
 
       -- Approach 2. Using - Self-Join -- 
-SELECT pa1.player_id, pa1.event_date, SUM(pa2.games_played) games_played_so_far
-FROM playeractivity pa1
-JOIN playeractivity pa2 
-	ON pa1.player_id = pa2.player_id AND pa1.event_date >= pa2.event_date
-GROUP BY pa1.player_id, pa1.event_date
+SELECT a1.player_id, a1.event_date, SUM(a2.games_played) games_played_so_far
+FROM activity a1
+JOIN activity a2 
+	ON a1.player_id = a2.player_id AND a1.event_date >= a2.event_date
+GROUP BY a1.player_id, a1.event_date
