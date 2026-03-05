1169d8b09757753f20dc5d724967787ebdeebb8d
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-05 12:13:58 +0100

    Update 1098. Unpopular Books.sql

--- SQL/Medium/1098. Unpopular Books.sql
+++ SQL/Medium/1098. Unpopular Books.sql
@@ -1,20 +1,7 @@
 
 
-         -- Approach 1. Using - Subquery and multiple conditions in - WHERE -- 
-SELECT book_id, name 
-FROM books 
-WHERE available_from < (SELECT '2019-06-23'::date - INTERVAL '1 Month')
-    AND NOT EXISTS (
-        SELECT 1
-        FROM orders o
-        WHERE o.book_id = books.book_id
-        GROUP BY o.book_id, o.dispatch_date
-        HAVING SUM(o.quantity) >= 10
-    )
 
-
-
-         -- Approach 2. Using - BETWEEN - instead of - Subquery -- 
+         -- Approach 1. Using - NOT EXISTS condition -- 
 SELECT b.book_id, b.name
 FROM books b
 WHERE b.available_from < ('2019-06-23'::date - INTERVAL '1 month')
