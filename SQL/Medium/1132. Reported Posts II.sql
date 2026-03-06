0f9b93bf2e3ceedba7b31b0e8dd63c41a52def78
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-06 13:26:27 +0100

    Update 1132. Reported Posts II.sql

--- SQL/Medium/1132. Reported Posts II.sql
+++ SQL/Medium/1132. Reported Posts II.sql
@@ -1,18 +1,7 @@
 
 
-         -- Approach 1. Using - Subquery with - LEFT JOIN / GROUP BY / COUNT(DISTINCT...) --   
-SELECT ROUND(AVG(romove_rate), 2) avg_remove_rate 
-FROM (
-  SELECT COUNT(DISTINCT r.post_id) * 100.0 / COUNT(DISTINCT a.post_id) romove_rate
-  FROM actions a
-  LEFT JOIN removals r ON r.post_id = a.post_id
-  WHERE a.extra = 'spam'
-  GROUP BY a.action_date
-) sub; 
 
-
-
-         -- Approach 2. Using - CTE -- 
+         -- Approach 1. Using - CTE -- 
 WITH daily_rate AS (
     SELECT a.action_date,
         COUNT(DISTINCT r.post_id) * 100.0 / COUNT(DISTINCT a.post_id) remove_rate
@@ -21,5 +10,5 @@ WITH daily_rate AS (
     WHERE a.extra = 'spam'
     GROUP BY a.action_date
 )
-SELECT ROUND(AVG(romove_rate), 2) avg_remove_rate 
+SELECT ROUND(AVG(remove_rate), 2) average_daily_percent  
 FROM daily_rate;
