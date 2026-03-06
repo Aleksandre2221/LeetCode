6373c79c9f5a612d7fb00c40d9ce46ed3828d881
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-06 11:30:44 +0100

    Update 1149. Article Views II.sql

--- SQL/Medium/1149. Article Views II.sql
+++ SQL/Medium/1149. Article Views II.sql
@@ -1,21 +1,8 @@
 
 
-         -- Approach 1. Using - CTE -- 
-WITH groupped AS (
-  SELECT article_id, viewer_id, view_date
-  FROM articleview 
-  GROUP BY article_id, viewer_id, view_date 
-)
-SELECT viewer_id 
-FROM groupped  
-GROUP BY viewer_id, view_date
-HAVING COUNT(*) > 1;
-
-
-
-
-       -- Approach 2. Using - DISTINCT ON condition (ONLY PostgreSQL) -- 
-SELECT viewer_id 
-FROM (SELECT DISTINCT ON (viewer_id, article_id) * FROM articleview) sub 
-GROUP BY viewer_id, view_date
-HAVING COUNT(*) > 1;
+         -- Approach 1. Using - GROUP BY with - HAVING -- 
+SELECT DISTINCT viewer_id id 
+FROM views 
+GROUP BY viewer_id, view_date 
+HAVING COUNT(DISTINCT article_id) > 1
+ORDER BY id;
