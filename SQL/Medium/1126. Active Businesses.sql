804e1f8b7f604340405fc894fe1325285a4b879d
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-06 09:54:18 +0100

    Update 1126. Active Businesses.sql

--- SQL/Medium/1126. Active Businesses.sql
+++ SQL/Medium/1126. Active Businesses.sql
@@ -1,32 +1,17 @@
 
 
-         -- Approach 1. Using - Subquery with Window Function - AVG -- 
-SELECT business_id
-FROM (
-  SELECT *, 
-  	AVG(occurences) OVER(PARTITION BY event_type) avg_occurences 
-  FROM businessevents
-) sub 
-WHERE occurences > avg_occurences
-GROUP BY business_id
-HAVING COUNT(*) > 1;
 
-
-
-
-         -- Approach 2. Using - CTE with - GROUP BY and - AVG -- 
-WITH avg_occur AS (
-  SELECT event_type, AVG(occurences) avg_occurences 
-  FROM businessevents
-  GROUP BY event_type
+         -- Approach 1. Using - CTE with - GROUP BY and - AVG -- 
+WITH events_avg AS (
+    SELECT *, 
+        AVG(occurrences) OVER(PARTITION BY event_type) eve_avg
+    FROM events 
 )
 SELECT business_id 
-FROM businessevents b
-JOIN avg_occur ON avg_occur.event_type = b.event_type
-WHERE  b.occurences > avg_occur.avg_occurences
+FROM events_avg 
+WHERE occurrences > eve_avg
 GROUP BY business_id
-HAVING COUNT(*) > 1; 
-
+HAVING COUNT(*) > 1;
 
 
 
