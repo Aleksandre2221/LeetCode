fda441d3207f7fb539367d6538dc70b88f180b9a
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-06 13:35:45 +0100

    Update 1142. User Activity for the Past 30 Days II.sql

--- SQL/Easy/1142. User Activity for the Past 30 Days II.sql
+++ SQL/Easy/1142. User Activity for the Past 30 Days II.sql
@@ -1,10 +1,23 @@
 
 
-         -- The Best Approach. Using - Subquery with - WHERE BETWEEN condition and - GROUP BY / COUNT(DISTINCT..) --  
-SELECT NULLIF(ROUND(AVG(cnt), 2), 0) AS average_sessions_per_user
+
+         -- Approach 1. Using - Subquery -- 
+SELECT COALESCE(ROUND(AVG(cnt), 2), 0) AS average_sessions_per_user
 FROM (
   SELECT COUNT(DISTINCT session_id) cnt 
-  FROM activities 
+  FROM activity 
   WHERE activity_date BETWEEN '2019-06-27'::date AND '2019-07-27'::date
   GROUP BY user_id
 ) sub;
+
+
+         -- Approach 2. Using one query -- 
+SELECT 
+    COALESCE(
+        ROUND(
+                COUNT(DISTINCT session_id)::numeric 
+                / NULLIF(COUNT(DISTINCT user_id), 0)
+        , 2)
+    , 0) average_sessions_per_user
+FROM activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27';
