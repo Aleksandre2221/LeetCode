9f232aeb1be9233a33be03748e4d1622e114f634
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-11 10:34:24 +0100

    Update 2504 - Concatenate the Name and the Profession.sql

--- SQL/Easy/2504 - Concatenate the Name and the Profession.sql
+++ SQL/Easy/2504 - Concatenate the Name and the Profession.sql
@@ -3,11 +3,13 @@
          -- Approach 1. Using - CONCAT -- 
 SELECT person_id,
     CONCAT(name, '(', LEFT(profession::text, 1), ')') name
-FROM person;
+FROM person
+ORDER BY peson_id DESC;
 
 
          -- Approach 2. Using - || operator -- 
 SELECT person_id, 
     name || '(' || LEFT(profession::text, 1) || ')' name 
-FROM person;
+FROM person
+ORDER BY peson_id DESC;
 
