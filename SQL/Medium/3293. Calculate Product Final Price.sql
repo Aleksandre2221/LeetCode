6251f7357d3d956e7a0011dae2d94574cf813b59
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-11 12:47:05 +0100

    Update 3293. Calculate Product Final Price.sql

--- SQL/Medium/3293. Calculate Product Final Price.sql
+++ SQL/Medium/3293. Calculate Product Final Price.sql
@@ -1,8 +1,10 @@
 
 
          -- Approach 1. Using - LEFT JOIN --
-SELECT p.product_id, p.category, 
-  COALESCE(p.price * (1 - d.discount/100.0), p.price) price
+SELECT 
+    p.product_id,
+    p.price - COALESCE((p.price / 100.0 * d.discount), 0) final_price, 
+    p.category 
 FROM products p 
-LEFT JOIN discounts d ON d.category = p.category
-ORDER BY p.product_id;
+LEFT JOIN discounts d USING (category)
+ORDER BY product_id;
