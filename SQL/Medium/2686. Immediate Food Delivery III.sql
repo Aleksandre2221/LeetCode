d2376d06f33388c91c629a4dd417bb7109f1e52a
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-11 12:29:34 +0100

    Update 2686. Immediate Food Delivery III.sql

--- SQL/Medium/2686. Immediate Food Delivery III.sql
+++ SQL/Medium/2686. Immediate Food Delivery III.sql
@@ -1,11 +1,12 @@
 
 
          -- Approach 1. Using - SUM with - CASW...WHEN conditions -- 
-SELECT order_date,
-  COALESCE(
-      ROUND(
-            SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) * 100.0 / COUNT(*)
-        , 2)
-    , 0) immediate_percentage 
-FROM delivery 
-GROUP BY order_date; 
+SELECT 
+    order_date,
+    ROUND(
+        SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+    ,2) immediate_percentage
+FROM delivery
+GROUP BY order_date
+ORDER BY order_date;
