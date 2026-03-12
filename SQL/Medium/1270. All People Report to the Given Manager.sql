ebaa7db4dbee6c4d0d27bf7032c93d0fbd7dc7a8
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-12 11:37:38 +0100

    Update 1270. All People Report to the Given Manager.sql

--- SQL/Medium/1270. All People Report to the Given Manager.sql
+++ SQL/Medium/1270. All People Report to the Given Manager.sql
@@ -2,8 +2,9 @@
 
          -- Approach 1. Using multiple - JOIN and - WHERE condition -- 
 SELECT e1.employee_id 
-FROM employee e1 
-JOIN employee e2 ON e1.manager_id = e2.employee_id
-JOIN employee e3 ON e2.manager_id = e3.employee_id
-WHERE e3.manager_id = 1 and e1.employee_id <> 1;
+FROM employees e1 
+JOIN employees e2 ON e1.manager_id = e2.employee_id
+JOIN employees e3 ON e2.manager_id = e3.employee_id
+WHERE e1.employee_id <> e1.manager_id 
+         AND e3.manager_id = 1;
 
