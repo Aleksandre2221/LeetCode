ee169e651a4d0590894f8bd9a608ac1d5c4dc57f
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-12 12:22:14 +0100

    Update 1355. Activity Participants.sql

--- SQL/Medium/1355. Activity Participants.sql
+++ SQL/Medium/1355. Activity Participants.sql
@@ -1,24 +1,13 @@
 
 
-         -- Approach 1. Using - CTE and - WHERE condtions -- 
-WITH counts AS (
-  SELECT activity, COUNT(*) cnt 
-  FROM friends
-  GROUP BY activity
+         -- Approach 1. Using - CTE with Window Functions --
+WITH ranking AS (
+    SELECT activity, 
+        RANK() OVER(ORDER BY COUNT(*)) min,  
+        RANK() OVER(ORDER BY COUNT(*) DESC) max
+    FROM friends 
+    GROUP BY activity
 )
 SELECT activity 
-FROM counts
-WHERE cnt <> (SELECT MIN(cnt) FROM counts)
-	AND cnt <> (SELECT MAX(cnt) FROM counts)
-
-
-
-
-         -- Approach 2. Using - GROUP BU with - HVING and - NOT IN condition --   
-SELECT activity 
-FROM friends
-GROUP BY activity
-HAVING COUNT(*) NOT IN (
-	(SELECT COUNT(*) FROM friends GROUP BY activity ORDER BY COUNT(*) LIMIT 1),
-	(SELECT COUNT(*) FROM friends GROUP BY activity ORDER BY COUNT(*) DESC LIMIT 1)
-);
+FROM ranking 
+WHERE min <> 1 AND max <> 1;
