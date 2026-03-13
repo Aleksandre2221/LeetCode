add79393ba3aed9fdd059eaaf43cd2c781fddee0
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-13 11:14:10 +0100

    Update 1398. Customers Who Bought Products A and B but Not C.sql

--- SQL/Medium/1398. Customers Who Bought Products A and B but Not C.sql
+++ SQL/Medium/1398. Customers Who Bought Products A and B but Not C.sql
@@ -8,14 +8,7 @@ GROUP BY c.customer_id, c.customer_name
 HAVING 
     SUM(CASE WHEN o.product_name = 'A' THEN 1 ELSE 0 END) > 0 AND
     SUM(CASE WHEN o.product_name = 'B' THEN 1 ELSE 0 END) > 0 AND
-    SUM(CASE WHEN o.product_name = 'C' THEN 1 ELSE 0 END) = 0;
+    SUM(CASE WHEN o.product_name = 'C' THEN 1 ELSE 0 END) = 0
+ORDER BY customer_id;
 
 
-
-         -- Approach 2. Using - WHERE with multiple - IN / NOT IN conditions -- 
-SELECT customer_id, customer_name
-FROM customers
-WHERE  
-	customer_id IN (SELECT customer_id FROM orders WHERE product_name = 'A') AND 
-	customer_id IN (SELECT customer_id FROM orders WHERE product_name = 'B') AND 
-  customer_id NOT IN (SELECT customer_id FROM orders WHERE product_name = 'C')
