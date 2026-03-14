f1411a835d8b877521037febd60923c39dfae1f5
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-14 10:15:02 +0100

    Update 1571. Warehouse Manager.sql

--- SQL/Easy/1571. Warehouse Manager.sql
+++ SQL/Easy/1571. Warehouse Manager.sql
@@ -1,8 +1,17 @@
 
 
-         -- The Best Approach. Using - JOIN and - GROUP BY -- 
-SELECT w.name warehouse_name, 
-	SUM(w.units * p.width * p.length * p.height) volume
-FROM warehouse w 
-JOIN products p ON w.product_id = p.product_id
-GROUP BY w.name; 
+         -- Approach 1. Using - JOIN and - GROUP BY -- 
+WITH warehouse_product AS (
+    SELECT 
+        w.name, 
+        p.product_id, 
+        SUM(p.width * p.length * p.height * w.units) total
+    FROM warehouse w 
+    JOIN products p USING (product_id)
+    GROUP BY w.name, p.product_id
+)
+SELECT 
+    name warehouse_name, 
+    SUM(total) volume
+FROM warehouse_product
+GROUP BY name;
