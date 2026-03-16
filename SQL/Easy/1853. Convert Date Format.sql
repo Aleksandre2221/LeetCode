130e96f0a2ad3c1a278ba3422d682afe9f5afc16
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-16 11:18:08 +0100

    Update 1853. Convert Date Format.sql

--- SQL/Easy/1853. Convert Date Format.sql
+++ SQL/Easy/1853. Convert Date Format.sql
@@ -1,10 +1,10 @@
 
 
          -- Approach 1. Using - TO_CHAR (For Postgre/Oracle) -- 
-SELECT TO_CHAR("day", 'Day, Month DD, YYYY') "DAY"
-FROM days
+SELECT TO_CHAR(day, 'FMDay, FMMonth FMDD, YYYY') "day"
+FROM days;
 
 
          -- Approach 2. Using - DATE_FORMAT (For MySQL) -- 
-SELECT DATE_FORMAT(`day`, '%W, %M %d, %Y') DAY
+SELECT DATE_FORMAT(day, '%W, %M %e, %Y') AS day
 FROM days;
