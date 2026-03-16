c82f183533db8756ecc03671fefffb9140b7cc66
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-16 11:02:06 +0100

    Update 1821. Find Customers With Positive Revenue this Year.sql

--- SQL/Easy/1821. Find Customers With Positive Revenue this Year.sql
+++ SQL/Easy/1821. Find Customers With Positive Revenue this Year.sql
@@ -1,8 +1,6 @@
 
 
-         -- The Best Approach. Using - WHERE / GROUP BY / HAVING -- 
-SELECT customer_id 
-FROM customers
-WHERE year = 2021
-GROUP BY customer_id
-HAVING SUM(revenue) > 0;
+         -- Approach 1. Using - WHERE conditions -- 
+SELECT customer_id
+FROM customers 
+WHERE year = 2021 AND revenue > 0;
