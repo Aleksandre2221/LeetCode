762fc5111c0e1ccf7476b3319f506a61b190e826
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-17 11:53:14 +0100

    Update 3172. Second Day Verification.sql

--- SQL/Easy/3172. Second Day Verification.sql
+++ SQL/Easy/3172. Second Day Verification.sql
@@ -1,9 +1,12 @@
 
 
-         -- The Best Approach. Using - JOIN with multiple conditions --  
+         -- Approach 1. Using - WHERE condtions -- 
 SELECT e.user_id
 FROM emails e 
-JOIN texts t ON t.email_id = e.email_id 
-	AND t.action_date - e.signup_date <= INTERVAL '1 Day'
-  AND t.signup_action = 'Verified';  
+JOIN texts t 
+    ON e.email_id = t.email_id 
+    AND t.signup_action = 'Verified'
+    AND t.action_date > e.signup_date
+    AND t.action_date <= e.signup_date + INTERVAL '1 Day'
+ORDER BY e.user_id;
   
