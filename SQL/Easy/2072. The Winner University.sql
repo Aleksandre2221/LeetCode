eb309572dd5d00a74ad5de53be244401c090820a
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-17 12:10:45 +0100

    Update 2072. The Winner University.sql

--- SQL/Easy/2072. The Winner University.sql
+++ SQL/Easy/2072. The Winner University.sql
@@ -3,10 +3,10 @@
          -- The Best Approach. Using - Cross Join and - CASE...WHEN conditions --  
 SELECT
 	CASE  
-    	WHEN n.cnt > c.cnt THEN 'New York University' 
-      WHEN c.cnt < n.cnt THEN 'California University'
-      ELSE 'No Winner'
-  END winner
+        WHEN n.cnt > c.cnt THEN 'New York University' 
+        WHEN n.cnt < c.cnt THEN 'California University'
+        ELSE 'No Winner'
+    END winner
 FROM 
 	(SELECT COUNT(*) cnt FROM newyork WHERE score >= 90) n, 
-  (SELECT COUNT(*) cnt FROM california WHERE score >= 90) c
+    (SELECT COUNT(*) cnt FROM california WHERE score >= 90) c;
