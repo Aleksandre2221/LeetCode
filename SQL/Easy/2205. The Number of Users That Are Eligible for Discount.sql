99f5c8cfb2cc017587873411ba806e05538eae96
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-17 11:27:39 +0100

    Update 2205. The Number of Users That Are Eligible for Discount.sql

--- SQL/Easy/2205. The Number of Users That Are Eligible for Discount.sql
+++ SQL/Easy/2205. The Number of Users That Are Eligible for Discount.sql
@@ -1,7 +1,15 @@
 
 
-         -- The Best Approach. Using multiple - WHERE conditions and - COUNT(DISTUNCT) -- 
-SELECT COUNT(DISTINCT user_id) AS user_count
-FROM Purchases
-WHERE time_stamp >= startDate AND time_stamp < endDate + INTERVAL '1 day'
-  AND amount >= minAmount;
+         -- Approach 1. Using - WHERE conditions -- 
+CREATE OR REPLACE FUNCTION getUserIDs(startDate TIMESTAMP, endDate TIMESTAMP, minAmount INT) RETURNS INT AS $$
+BEGIN
+  RETURN (
+    SELECT COUNT(DISTINCT user_id) AS user_count
+    FROM Purchases
+    WHERE 
+        time_stamp >= startDate 
+        AND time_stamp BETWEEN startDate AND endDate
+        AND amount >= minAmount
+  );
+END;
+$$ LANGUAGE plpgsql;
