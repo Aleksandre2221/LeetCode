e04c75772bb4af0ca3bc7c7f1136d23b21494083
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-18 10:27:06 +0100

    Update 1867. Orders With Maximum Quantity Above Average.sql

--- SQL/Medium/1867. Orders With Maximum Quantity Above Average.sql
+++ SQL/Medium/1867. Orders With Maximum Quantity Above Average.sql
@@ -1,7 +1,12 @@
 
 
-         -- The Best Approach. Using - GROUP BY with - HAVING condition --  
-SELECT order_id 
-FROM ordersdetails 
-GROUP BY order_id 
-HAVING MAX(quantity) > (SELECT avg(quantity) FROM ordersdetails);
+         -- Approach 1. Using - CTE --   
+WITH calcs AS (
+    SELECT order_id, product_id, 
+        AVG(quantity) OVER(PARTITION BY order_id) avg_q, 
+        MAX(quantity) OVER(PARTITION BY order_id) max_q
+    FROM ordersdetails
+)
+SELECT DISTINCT order_id 
+FROM calcs 
+WHERE max_q > (SELECT MAX(avg_q) FROM calcs);
