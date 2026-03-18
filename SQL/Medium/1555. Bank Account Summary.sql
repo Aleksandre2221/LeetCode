38ce58b2bea7e484dd9139a12152d5b89ea22033
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-18 09:26:35 +0100

    Update 1555. Bank Account Summary.sql

--- SQL/Medium/1555. Bank Account Summary.sql
+++ SQL/Medium/1555. Bank Account Summary.sql
@@ -3,43 +3,25 @@
 -- Risolved: 4 times
 
 
-         -- Approach 1. Using - UNION ALL within - LEFT JOIN -- 
-SELECT u.user_id, u.user_name,
-    u.credit + COALESCE(SUM(t.amount_change), 0) credit,
-    CASE 
-        WHEN u.credit + COALESCE(SUM(t.amount_change), 0) < 0 THEN 'Yes'
-        ELSE 'No'
-    END credit_limit_breached
-FROM users u
-LEFT JOIN (
-    SELECT paid_by AS user_id, -amount amount_change FROM transaction
-    UNION ALL
-    SELECT paid_to AS user_id,  amount amount_change FROM transaction
-) t ON t.user_id = u.user_id
-GROUP BY u.user_id, u.user_name;
-
-
-
-
-         -- Approach 2. Using two - CTE -- 
+         -- Approach 1. Using - UNION ALL --  
 WITH 
-    paid AS (
-        SELECT paid_by user_id, SUM(amount) total_paid
-        FROM transaction 
-        GROUP BY paid_by
-    ),
-    received AS (
-        SELECT paid_to user_id, SUM(amount) total_received
-        FROM transaction 
-        GROUP BY paid_to
+	all_trans AS (
+      SELECT paid_by AS user_id, -amount AS amount FROM transactions 
+      UNION ALL 
+      SELECT paid_to, amount FROM transactions 
+	),
+    total_amount AS (
+      SELECT user_id, SUM(amount) total 
+      FROM all_trans  
+      GROUP BY user_id
 )
-SELECT u.user_id, u.user_name,
-	u.credit - COALESCE(p.total_paid, 0) + COALESCE(r.total_received, 0) credit,
-    CASE 
-        WHEN (u.credit - COALESCE(p.total_paid, 0) + COALESCE(r.total_received, 0)) < 0 
-        THEN 'Yes'
-        ELSE 'No'
-    END credit_limit_breached
-FROM users u
-LEFT JOIN paid p ON p.user_id = u.user_id
-LEFT JOIN received r ON r.user_id = u.user_id;
+SELECT u.user_id, u.user_name, 
+	COALESCE(SUM(u.credit + ta.total), u.credit) credit, 
+	CASE 
+    	WHEN SUM(u.credit + ta.total) < 0 
+        THEN 'Yes' 
+        ELSE 'No' 
+	END credit_limit_breached
+FROM users u 
+LEFT JOIN total_amount ta USING (user_id)
+GROUP BY u.user_id, u.user_name, u.credit;
