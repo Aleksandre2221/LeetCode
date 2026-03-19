c624ae2694037ddc489349bb27120a2dd9840cf1
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-19 10:47:43 +0100

    Update 2324. Product Sales Analysis IV.sql

--- SQL/Medium/2324. Product Sales Analysis IV.sql
+++ SQL/Medium/2324. Product Sales Analysis IV.sql
@@ -1,37 +1,21 @@
 
 
-         -- Approach 1. Using - CTE with a Window Function - RANK() -- 
-WITH ranking AS (
-  SELECT s.user_id, s.product_id, 
-    RANK() OVER(PARTITION BY user_id ORDER BY SUM(s.quantity * p.price) DESC) rnk
-  FROM product p  
-  JOIN sales s ON s.product_id = p.product_id
-  GROUP BY s.user_id, s.product_id	
-)
-SELECT user_id, product_id 
-FROM ranking 
-WHERE rnk = 1;
-
-
-
-         -- Approach 2. Without using any Window Function -- 
+         -- Approach 1. Using two - CTE --  
 WITH 
-  totals AS (
-    SELECT s.user_id, s.product_id,
-      SUM(s.quantity * p.price) total_spent
-    FROM sales s
-    JOIN product p ON p.product_id = s.product_id
-    GROUP BY s.user_id, s.product_id
-	),
-  max_spent AS (
-    SELECT user_id, MAX(total_spent) max_value
-    FROM totals  
-    GROUP BY user_id
-)
-SELECT t.user_id, t.product_id
-FROM totals t
-JOIN max_spent m ON t.user_id = m.user_id 
-  AND t.total_spent = m.max_value;
+	total_amount AS (
+    	SELECT s.user_id, p.product_id, 
+            SUM(s.quantity * p.price) OVER(PARTITION BY s.user_id, p.product_id ) ta
+        FROM sales s 
+        JOIN product p USING (product_id)
+  	), 
+  	max_amount AS (
+        SELECT *, 
+      	    MAX(ta) OVER(PARTITION BY user_id) ma  
+        FROM total_amount
+) 
+SELECT DISTINCT user_id, product_id 
+FROM max_amount  
+WHERE ta = ma;
 
 
 
