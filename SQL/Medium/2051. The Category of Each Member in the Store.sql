e6f6503965da1c864ee5ca1c64c28b64b29e6bd5
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-19 09:44:35 +0100

    Update 2051. The Category of Each Member in the Store.sql

--- SQL/Medium/2051. The Category of Each Member in the Store.sql
+++ SQL/Medium/2051. The Category of Each Member in the Store.sql
@@ -1,14 +1,14 @@
 
 
-         -- The Best Approach. Using two - LEFT JOIN and - CASE...WHEN conditions -- 
-SELECT m.member_id, m.name,
-    CASE
-        WHEN COUNT(v.visit_id) = 0 THEN 'Bronze'
-        WHEN 100 * COUNT(p.visit_id) / COUNT(v.visit_id) >= 80 THEN 'Diamond'
-        WHEN 100 * COUNT(p.visit_id) / COUNT(v.visit_id) >= 50 THEN 'Gold'
-        ELSE 'Silver'
+         -- Approach 1. Using two - LEFT JOIN and - CASE...WHEN conditions -- 
+SELECT m.member_id, m.name, 
+    CASE 
+        WHEN COUNT(p.charged_amount) * 100.0 / NULLIF(COUNT(v.visit_id), 0) >= 80 THEN 'Diamond'
+        WHEN COUNT(p.charged_amount) * 100.0 / NULLIF(COUNT(v.visit_id), 0) BETWEEN 50 AND 79 THEN 'Gold'
+        WHEN COUNT(p.charged_amount) * 100.0 / NULLIF(COUNT(v.visit_id), 0) < 50 THEN 'Silver'
+        WHEN COUNT(v.visit_id) = 0 THEN 'Bronze' 
     END category
-FROM members m
-LEFT JOIN visits v ON m.member_id = v.member_id
-LEFT JOIN purchases p ON v.visit_id = p.visit_id
-GROUP BY m.member_id;
+FROM members m 
+LEFT JOIN visits v USING (member_id) 
+LEFT JOIN purchases p USING (visit_id)
+GROUP BY m.member_id, m.name;
