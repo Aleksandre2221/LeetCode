e66f7333bb867f7636dae1cc347fa0b93a2fad77
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-19 09:22:39 +0100

    Update 2020. Number of Accounts That Did Not Stream.sql

--- SQL/Medium/2020. Number of Accounts That Did Not Stream.sql
+++ SQL/Medium/2020. Number of Accounts That Did Not Stream.sql
@@ -1,9 +1,12 @@
 
 
-         -- Approach 1. Using - LEFT JOIN with multiple conditions -- 
-SELECT COUNT(*) 
-FROM subscriptions sub
-LEFT JOIN streams str ON str.account_id = sub.account_id 
-	AND str.stream_date NOT BETWEEN sub.start_date AND sub.end_date
-WHERE EXTRACT(YEAR FROM sub.start_date) = 2021; 
-
+         -- Approach 1. Using multiple  - WHERE conditions -- 
+SELECT COUNT(*) accounts_count 
+FROM subscriptions sub 
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM streams str
+    WHERE str.account_id = sub.account_id
+        AND EXTRACT(YEAR FROM stream_date) = 2021
+)
+AND (EXTRACT(YEAR FROM start_date) = 2021 OR EXTRACT(YEAR FROM end_date) = 2021);
