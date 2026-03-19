e7c917c7eddf4111263117f065972fec174d16fd
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-19 11:12:03 +0100

    Update 2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql

--- SQL/Medium/2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
+++ SQL/Medium/2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
@@ -12,17 +12,3 @@ WHERE
   OR o.order_type = 0;
 
 
-
-         -- Approach 2. Using - CTE -- 
-WITH type_o AS (
-  SELECT DISTINCT customer_id, 
-      CASE  
-          WHEN customer_id IN (SELECT DISTINCT customer_id FROM orders WHERE order_type = 0) THEN 0 
-          ELSE 1 
-      END order_type
-   FROM orders
-)
-SELECT o.order_id, o.customer_id, t.order_type
-FROM type_o t  
-JOIN orders o ON o.customer_id = t.customer_id AND o.order_type = t.order_type;
-
