708ee83e980e1b459fc979584906c3fe9e239bec
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-19 10:04:34 +0100

    Update 2314. The First Day of the Maximum Recorded Degree in Each City.sql

--- SQL/Medium/2314. The First Day of the Maximum Recorded Degree in Each City.sql
+++ SQL/Medium/2314. The First Day of the Maximum Recorded Degree in Each City.sql
@@ -6,9 +6,10 @@ WITH row_num AS (
     ROW_NUMBER() OVER(PARTITION BY city_id ORDER BY degree DESC, DAY ) rn
   FROM weather
 )
-SELECT city_id city, day, degree 
+SELECT city_id, day, degree 
 FROM row_num 
-WHERE rn = 1;
+WHERE rn = 1
+ORDER BY city_id;
 
 
 
