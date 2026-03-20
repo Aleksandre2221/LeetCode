06333b17e7b621fe2b3a512058352d86eb89cf96
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-20 10:03:24 +0100

    Update 3182. Find Top Scoring Students.sql

--- SQL/Medium/3182. Find Top Scoring Students.sql
+++ SQL/Medium/3182. Find Top Scoring Students.sql
@@ -7,4 +7,5 @@ JOIN courses c ON s.major = c.major
 LEFT JOIN enrollments e ON e.student_id = s.student_id
      AND e.course_id = c.course_id
 GROUP BY s.student_id
-HAVING SUM(CASE WHEN e.grade = 'A' THEN 1 ELSE 0 END) = COUNT(c.major);
+HAVING SUM(CASE WHEN e.grade = 'A' THEN 1 ELSE 0 END) = COUNT(c.major)
+ORDER BY student_id;
