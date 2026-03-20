69d483e1b23c2b161cdff9ea8483a604ae2b7f91
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-20 10:20:44 +0100

    Update 2112. The Airport With the Most Traffic.sql

--- SQL/Medium/2112. The Airport With the Most Traffic.sql
+++ SQL/Medium/2112. The Airport With the Most Traffic.sql
@@ -2,16 +2,17 @@
 
          -- Approach 1. Using two - CTE -- 
 WITH 
-	all_airports AS (
-  		SELECT departure_airport airport_id, flights_count FROM flights
-  		UNION ALL  
-  		SELECT arrival_airport, flights_count FROM flights
-	),
-    total_flights AS (
-      SELECT airport_id, SUM(flights_count) total 
-      FROM all_airports
-      GROUP BY airport_id
+    bidir AS (
+        SELECT departure_airport AS airport_id, flights_count FROM flights
+        UNION ALL 
+        SELECT arrival_airport, flights_count FROM flights
+    ),
+    ranking AS (
+        SELECT airport_id, 
+            RANK() OVER(ORDER BY SUM(flights_count) DESC) rnk 
+        FROM bidir
+        GROUP BY airport_id
 )
 SELECT airport_id
-FROM total_flights tf 
-WHERE total = (SELECT MAX(total) FROM total_flights);
+FROM ranking 
+WHERE rnk = 1;
