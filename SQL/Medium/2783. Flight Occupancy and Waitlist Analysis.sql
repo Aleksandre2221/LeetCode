880e995d7e7dfc13552c96ec493866ea5ef38bed
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-21 14:08:38 +0100

    Update 2783. Flight Occupancy and Waitlist Analysis.sql

--- SQL/Medium/2783. Flight Occupancy and Waitlist Analysis.sql
+++ SQL/Medium/2783. Flight Occupancy and Waitlist Analysis.sql
@@ -1,27 +1,20 @@
 
 
-         -- Approach 1. Using - LEAST / GREATEST -- 
-SELECT p.flight_id, 
-	LEAST(COUNT(*), MAX(f.capacity)) booked_cnt,
-	GREATEST(COUNT(*) - MAX(f.capacity), 0) waitlist_cnt 
-FROM passengers p
-JOIN flights f ON f.flight_id = p.flight_id
-GROUP BY p.flight_id
-ORDER BY flight_id;
 
-
-
-         -- Approach 2. Using - CASE...WHEN conditions -- 
-SELECT p.flight_id,
-    CASE 
-        WHEN COUNT(*) <= f.capacity THEN COUNT(*) 
-        ELSE f.capacity 
-    END booked_cnt,
-    CASE 
-        WHEN COUNT(*) > f.capacity THEN COUNT(*) - f.capacity
+         -- Approach 1. Using - CASE...WHEN conditions -- 
+SELECT 
+	f.flight_id, 
+    CASE  
+    	WHEN capacity >  COUNT(p.flight_id) AND COUNT(p.flight_id) > 0 THEN COUNT(p.flight_id)
+        WHEN capacity <= COUNT(p.flight_id) AND COUNT(p.flight_id) > 0 THEN capacity
         ELSE 0
-    END waitlist_cnt
-FROM passengers p
-JOIN flights f ON p.flight_id = f.flight_id
-GROUP BY p.flight_id, f.capacity
+	END booked_cnt, 
+    CASE  
+    	WHEN capacity < COUNT(p.flight_id) AND COUNT(p.flight_id) > 0
+        THEN COUNT(p.flight_id) - capacity
+        ELSE 0
+	END waitlist_cnt
+FROM flights f  
+LEFT JOIN passengers p ON f.flight_id = p.flight_id
+GROUP BY f.flight_id, f.capacity
 ORDER BY flight_id;
