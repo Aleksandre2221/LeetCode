26feb67a408cbfae10a2cf075e774a6f779343d3
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-22 21:06:31 +0100

    Update 2346. Compute the Rank as a Percentage.sql

--- SQL/Medium/2346. Compute the Rank as a Percentage.sql
+++ SQL/Medium/2346. Compute the Rank as a Percentage.sql
@@ -4,26 +4,12 @@
 SELECT student_id, department_id,
     COALESCE(
         ROUND(
-            	(RANK() OVER (PARTITION BY department_id ORDER BY mark DESC) - 1) * 100 
-                  / (COUNT(*) OVER (PARTITION BY department_id) - 1)
-            , 2
-        ), 0
-    ) percentage
+            	(RANK() OVER (PARTITION BY department_id ORDER BY mark DESC) - 1) * 100.0 
+                  / NULLIF((COUNT(*) OVER (PARTITION BY department_id) - 1), 0)
+            , 2)
+        , 0) percentage
 FROM students;
 
-
-
-        -- Approach 2. Using - CTE with Window Functions -- (more datailed) -- 
-WITH ranking AS (
-  SELECT *, 
-      DENSE_RANK() OVER(PARTITION BY department_id ORDER BY mark DESC) rnk, 
-      COUNT(*) OVER(PARTITION BY department_id) cnt  
-  FROM students
-) 
-SELECT student_id, department_id, 
-   MAX(ROUND((rnk - 1) * 100.0 / (cnt - 1), 2)) percentage
-FROM ranking 
-GROUP BY student_id, department_id;
 
 
 
