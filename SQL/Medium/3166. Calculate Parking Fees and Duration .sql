5d38e583ea0328dcb3933c41491421f6e26755e2
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-22 20:55:52 +0100

    Update 3166. Calculate Parking Fees and Duration .sql

--- SQL/Medium/3166. Calculate Parking Fees and Duration .sql
+++ SQL/Medium/3166. Calculate Parking Fees and Duration .sql
@@ -1,15 +1,24 @@
 
 
          -- Approach 1. Using - CTE with a Window Function - RANK() -- 
-WITH hours_info AS (
-  SELECT *, 
-    ROUND(EXTRACT(EPOCH FROM exit_time - entry_time) / 3600.0, 2) parking_time,  
-    RANK() OVER(PARTITION BY car_id ORDER BY exit_time - entry_time DESC) rnk
-  FROM parkingtransactions
+WITH 
+    calc AS (
+      SELECT car_id, 
+          SUM(fee_paid) total_fee_paid, 
+          ROUND(
+              SUM(fee_paid) / (EXTRACT(EPOCH FROM SUM(exit_time - entry_time)) / 3600)
+          , 2) avg_hourly_fee 
+      FROM parkingtransactions 
+      GROUP BY car_id
+    ), 
+    slot_time AS (
+      SELECT car_id, lot_id,
+      	 RANK() OVER(PARTITION BY car_id ORDER BY SUM(exit_time - entry_time) DESC) rnk
+      FROM parkingtransactions 
+      GROUP BY car_id, lot_id
 )
-SELECT car_id, 
-  ROUND(SUM(fee_paid), 2) total_fee_paid,
-  ROUND(SUM(fee_paid) / SUM(parking_time), 2) avg_hourly_fee,
-  MAX(CASE WHEN rnk = 1 THEN lot_id END) most_time_lot
-FROM hours_info
-GROUP BY car_id;
+SELECT c.car_id, c.total_fee_paid, c.avg_hourly_fee, st.lot_id most_time_lot   
+FROM calc c 
+JOIN slot_time st USING (car_id) 
+WHERE st.rnk = 1
+ORDER BY car_id; 
