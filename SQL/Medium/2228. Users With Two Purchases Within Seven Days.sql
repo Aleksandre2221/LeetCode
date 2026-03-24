5cf07921fb3e2afe7c6e180a7d6b24ede5e1e039
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-24 12:20:18 +0100

    Update 2228. Users With Two Purchases Within Seven Days.sql

--- SQL/Medium/2228. Users With Two Purchases Within Seven Days.sql
+++ SQL/Medium/2228. Users With Two Purchases Within Seven Days.sql
@@ -2,11 +2,11 @@
 
          -- Approach 1. Using - CTE with a Window Function - LAG() -- 
 WITH date_diff AS (
-  SELECT *, 
+  SELECT user_id, 
       purchase_date - LAG(purchase_date) OVER(PARTITION BY user_id ORDER BY purchase_date) diff
   FROM purchases
-  ORDER BY user_id, purchase_date
 )
-SELECT user_id 
+SELECT DISTINCT user_id 
 FROM date_diff
-WHERE diff <= 7;
+WHERE diff <= 7
+ORDER BY user_id;
