c0450e0f9c5a680f31239e8616c73194b0016c57
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-25 11:14:48 +0100

    Update 3050. Pizza Toppings Cost Analysis.sql

--- SQL/Medium/3050. Pizza Toppings Cost Analysis.sql
+++ SQL/Medium/3050. Pizza Toppings Cost Analysis.sql
@@ -2,8 +2,8 @@
 
          -- Approach 1. Using two - CROSS JOIN --  
 SELECT 
-    CONCAT(t1.topping_name, ',', t2.topping_name, ',', t3.topping_name) AS pizza, 
-    (t1.cost + t2.cost + t3.cost) AS total_cost
+    CONCAT(t1.topping_name, ',', t2.topping_name, ',', t3.topping_name) pizza, 
+    (t1.cost + t2.cost + t3.cost) total_cost
 FROM toppings t1 
 CROSS JOIN toppings t2
 CROSS JOIN toppings t3
