5c096b603b362d7c80a3f14003ce9d279482e363
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-26 10:32:34 +0100

    Update 1479. Sales by Day of the Week.sql

--- SQL/Hard/1479. Sales by Day of the Week.sql
+++ SQL/Hard/1479. Sales by Day of the Week.sql
@@ -2,22 +2,16 @@
 
 
          -- Approach 1. Using - CTE -- 
-WITH sold_amount AS (
-    SELECT i.item_category,
-        TRIM(TO_CHAR(o.order_date, 'Day')) week_day,
-        SUM(o.quantity) total_sold
-    FROM items i
-    LEFT JOIN orders o USING (item_id)
-    GROUP BY i.item_category, TRIM(TO_CHAR(o.order_date, 'Day'))
-)
-SELECT item_category,
-    SUM(CASE WHEN week_day = 'Monday'    THEN total_sold ELSE 0 END) "Monday",
-    SUM(CASE WHEN week_day = 'Tuesday'   THEN total_sold ELSE 0 END) "Tuesday",
-    SUM(CASE WHEN week_day = 'Wednesday' THEN total_sold ELSE 0 END) "Wednesday",
-    SUM(CASE WHEN week_day = 'Thursday'  THEN total_sold ELSE 0 END) "Thursday",
-    SUM(CASE WHEN week_day = 'Friday'    THEN total_sold ELSE 0 END) "Friday",
-    SUM(CASE WHEN week_day = 'Saturday'  THEN total_sold ELSE 0 END) "Saturday",
-    SUM(CASE WHEN week_day = 'Sunday'    THEN total_sold ELSE 0 END) "Sunday"
-FROM sold_amount
-GROUP BY item_category
-ORDER BY item_category;
+SELECT 
+    i.item_category "CATEGORY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 1 THEN o.quantity ELSE 0 END) "MONDAY",
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 2 THEN o.quantity ELSE 0 END) "TUESDAY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 3 THEN o.quantity ELSE 0 END) "WEDNESDAY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 4 THEN o.quantity ELSE 0 END) "THURSDAY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 5 THEN o.quantity ELSE 0 END) "FRIDAY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 6 THEN o.quantity ELSE 0 END) "SATURDAY", 
+    SUM(CASE WHEN EXTRACT(DOW FROM order_date) = 0 THEN o.quantity ELSE 0 END) "SUNDAY"
+FROM items i 
+LEFT JOIN orders o USING (item_id)
+GROUP BY i.item_category
+ORDER BY "CATEGORY";
