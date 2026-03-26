92f97448807b111149ccd1dccbe6b33aff657b85
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-26 11:13:48 +0100

    Update 2199. Finding the Topic of Each Post.sql

--- SQL/Hard/2199. Finding the Topic of Each Post.sql
+++ SQL/Hard/2199. Finding the Topic of Each Post.sql
@@ -4,24 +4,23 @@
          -- Approach 1. Using multiple - CTE -- 
 WITH 
 	words AS (
-    SELECT *, UNNEST(STRING_TO_ARRAY(content, ' ')) word
-    FROM posts
-  ),
-  grps AS (
-    SELECT DISTINCT w.post_id, k.topic_id  
-    FROM words w  
-    JOIN keywords k ON LOWER(w.word) = LOWER(k.word)
-	),
-  topics AS (
-    SELECT post_id, 
-      STRING_AGG(topic_id::text, ',' ORDER BY topic_id) topic 
-    FROM grps  
-    GROUP BY post_id
-)
-SELECT p.post_id, 
-	COALESCE(t.topic, 'Ambiguous!') topic
-FROM posts p  
-LEFT JOIN topics t ON p.post_id = t.post_id;
+      SELECT post_id, 
+      	 UNNEST(STRING_TO_ARRAY(content, ' ')) word
+      FROM posts
+	), 
+    topics AS (
+      SELECT DISTINCT w.post_id, k.topic_id
+      FROM words w  
+      JOIN keywords k ON LOWER(w.word) = LOWER(k.word)
+) 
+SELECT 
+	p.post_id, 
+    COALESCE(
+		STRING_AGG(t.topic_id::text, ',' ORDER BY t.topic_id)
+    , 'Ambiguous!') topic 
+FROM posts p 
+LEFT JOIN topics t USING (post_id)
+GROUP BY p.post_id;
 
 
 
