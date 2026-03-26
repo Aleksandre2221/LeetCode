66c21b9736c9f326965d758b96bf5c50860c1d3f
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-26 12:32:05 +0100

    Update 3214. Year on Year Growth Rate .sql

--- SQL/Hard/3214. Year on Year Growth Rate .sql
+++ SQL/Hard/3214. Year on Year Growth Rate .sql
@@ -1,26 +1,24 @@
 
 
 
-         -- Approach 1. Using multiple - CTE -- 
-WITH 
-	cur_spends AS (
+         -- Approach 1. Using - CTE -- 
+WITH year_sales AS (
     SELECT 
-        EXTRACT(YEAR FROM transaction_date) AS year, 
+        EXTRACT(YEAR FROM transaction_date) "Year", 
         product_id, 
-        SUM(spend) current_year_spend
+        SUM(spend) curr_year_spend
     FROM user_transactions 
     GROUP BY EXTRACT(YEAR FROM transaction_date), product_id
-	),
-  cur_prev_spends AS (
-    SELECT *, 
-        LAG(current_year_spend) OVER(PARTITION BY product_id ORDER BY YEAR) prev_year_spend
-    FROM cur_spends
 )
 SELECT 
-  	year, 
-    product_id,
-    current_year_spend, 
-    prev_year_spend, 
-    ROUND((current_year_spend - prev_year_spend) * 100.0 / prev_year_spend, 2) yoy_rate
-FROM cur_prev_spends
-ORDER BY product_id, year;
+	"Year", 
+    product_id, 
+    curr_year_spend, 
+    LAG(curr_year_spend) OVER(PARTITION BY product_id ORDER BY "Year") prev_year_spend,
+    ROUND(
+	        (curr_year_spend - LAG(curr_year_spend) OVER(PARTITION BY product_id ORDER BY "Year")) * 100.0
+	        / LAG(curr_year_spend) OVER(PARTITION BY product_id ORDER BY "Year") 
+     , 2) yoy_rate
+FROM year_sales  
+GROUP BY "Year", product_id, curr_year_spend
+ORDER BY product_id, "Year";
