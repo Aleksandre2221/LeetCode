02b6569a253a173f535b18d354fad4ed78ffc34a
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-26 12:17:27 +0100

    Update 3390. Longest Team Pass Streak.sql

--- SQL/Hard/3390. Longest Team Pass Streak.sql
+++ SQL/Hard/3390. Longest Team Pass Streak.sql
@@ -3,45 +3,37 @@
 
          -- Approach 1. Using multiple - CTE -- 
 WITH 
-	flagged AS (
-      SELECT 
-          t1.team_name team_name,
-          p.time_stamp,
-          CASE 
-              WHEN t1.team_name = t2.team_name 
-      		    THEN 1
-              ELSE 0
-          END is_success
-      FROM passes p
-      JOIN teams t1 ON t1.player_id = p.pass_from
-      JOIN teams t2 ON t2.player_id = p.pass_to
-  ),
-  filtered AS (
-      SELECT *
-      FROM flagged
-      WHERE is_success = 1
-  ),
-  row_nums AS (
-      SELECT 
-         team_name,
-         time_stamp,
-         ROW_NUMBER() OVER (ORDER BY time_stamp) global_rn,
-         ROW_NUMBER() OVER (PARTITION BY team_name ORDER BY time_stamp) team_rn
-      FROM filtered
-  ),
-  grps AS (
-      SELECT *,
-          (global_rn - team_rn) grp_id
-      FROM row_nums
-  ),
-  streaks_cnt AS (
-      SELECT team_name, 
-          COUNT(*) streak_length
-      FROM grps
-      GROUP BY team_name, grp_id
+	team_passes AS (
+        SELECT
+            t1.team_name,
+            p.time_stamp,
+            (t1.team_name = t2.team_name) intra_team
+        FROM Passes p
+        JOIN Teams t1 ON p.pass_from = t1.player_id
+        JOIN Teams t2 ON p.pass_to = t2.player_id
+    ),
+	grps AS (
+        SELECT
+            team_name,
+            time_stamp,
+            intra_team,
+            ROW_NUMBER() OVER (PARTITION BY team_name ORDER BY time_stamp) -
+            ROW_NUMBER() OVER (PARTITION BY team_name, intra_team ORDER BY time_stamp) grp
+        FROM team_passes
+    ),
+	streaks_ranking AS (
+        SELECT
+            team_name,
+            COUNT(*) AS cnt,
+            RANK() OVER (PARTITION BY team_name ORDER BY COUNT(*) DESC) rnk
+        FROM grps
+        WHERE intra_team = TRUE
+        GROUP BY team_name, grp
 )
-SELECT team_name,
-    MAX(streak_length) longest_streak
-FROM streaks_cnt
+SELECT
+    team_name,
+    MAX(cnt) AS longest_streak       
+FROM streaks_ranking
+WHERE rnk = 1
 GROUP BY team_name
 ORDER BY team_name;
