db682a26dd6bca9231b45c2546a159c9e2d1ed86
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-27 11:39:13 +0100

    Update 3052. Maximize Items.sql

--- SQL/Hard/3052. Maximize Items.sql
+++ SQL/Hard/3052. Maximize Items.sql
@@ -3,29 +3,25 @@
 
          -- Approach 1. Using multuple - CTE -- 
 WITH 
-  summary AS (
-    SELECT item_type, 
-        SUM(square_footage) total_sq, 
-        COUNT(*) item_count
-    FROM inventory
-    GROUP BY item_type
-  ),
 	prime AS (
-    SELECT item_type,
-        (FLOOR(500000 / total_sq) * item_count) item_count,
-        (500000 % total_sq) remaining_sq
-    FROM summary
-    WHERE item_type = 'prime_eligible'
-  ),
-	not_prime AS (
-	  SELECT item_type,
-    	  FLOOR((SELECT remaining_sq FROM prime) / total_sq) * item_count AS item_count
-    FROM summary
-    WHERE item_type = 'not_prime'
+      SELECT item_type,
+      	 COUNT(DISTINCT item_id) * FLOOR(500000 / SUM(square_footage)) item_count, 
+      	 SUM(square_footage) * FLOOR(500000 / SUM(square_footage)) total_occupied,
+      	 500000 - SUM(square_footage) * FLOOR(500000 / SUM(square_footage)) remaining
+      FROM inventory 
+      WHERE item_type = 'prime_eligible'
+      GROUP BY item_type
+	), 
+    not_prime AS (
+      SELECT item_type,
+      	 FLOOR((SELECT remaining FROM prime) / SUM(square_footage)) * COUNT(DISTINCT item_id) item_count
+      FROM inventory 
+      WHERE item_type = 'not_prime'
+      GROUP BY item_type
 )
 SELECT item_type, item_count FROM prime
-UNION ALL
-SELECT item_type, item_count FROM not_prime ORDER BY item_count DESC;
+UNION ALL 
+SELECT item_type, item_count FROM not_prime
 
 
 
