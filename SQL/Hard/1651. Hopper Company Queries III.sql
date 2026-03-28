c987789b3777ca7b5abe6f4a5035bca649c78ad6
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-28 10:42:31 +0100

    Update 1651. Hopper Company Queries III.sql

--- SQL/Hard/1651. Hopper Company Queries III.sql
+++ SQL/Hard/1651. Hopper Company Queries III.sql
@@ -9,14 +9,15 @@ WITH RECURSIVE months AS (
   FROM months  
   WHERE "month" + INTERVAL '1 month' <= '2020-10-01'::date
 ) 
-SELECT m.month,
-    ROUND(COALESCE(SUM(a.ride_distance), 0) / 3, 2) average_ride_distance,
-    ROUND(COALESCE(SUM(a.ride_duration), 0) / 3, 2) average_ride_duration
+SELECT 
+    EXTRACT(MONTH FROM m.month) "month",
+    ROUND(COALESCE(SUM(a.ride_distance), 0)::numeric / 3, 2) average_ride_distance,
+    ROUND(COALESCE(SUM(a.ride_duration), 0)::numeric / 3, 2) average_ride_duration
 FROM months m
 LEFT JOIN rides r
     ON EXTRACT(YEAR FROM r.requested_at) = 2020
     AND EXTRACT(MONTH FROM r.requested_at) BETWEEN EXTRACT(MONTH FROM m.month) AND EXTRACT(MONTH FROM m.month) + 2
 LEFT JOIN acceptedrides a 
     ON a.ride_id = r.ride_id
-GROUP BY m.month
-ORDER BY m.month;
+GROUP BY EXTRACT(MONTH FROM m.month)
+ORDER BY "month";
