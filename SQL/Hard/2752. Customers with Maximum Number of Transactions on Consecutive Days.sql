9a86bd9db37dfcf6b807699478babd6ce203b020
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-30 10:39:27 +0200

    Update 2752. Customers with Maximum Number of Transactions on Consecutive Days.sql

--- SQL/Hard/2752. Customers with Maximum Number of Transactions on Consecutive Days.sql
+++ SQL/Hard/2752. Customers with Maximum Number of Transactions on Consecutive Days.sql
@@ -4,17 +4,17 @@
         -- Approach 1. Using two - CTE -- 
 WITH 
 	cons_grps AS (
-      SELECT *, 
-        transaction_date - ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY transaction_date)::int cons_id
-      FROM transactions
-    ),
-    cons_cnt AS (
       SELECT customer_id, 
-      	COUNT(cons_id) cons_cnt
-      FROM cons_grps 
-      GROUP BY customer_id, cons_id
+      		transaction_date - ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY transaction_date)::int grp
+      FROM transactions
+	), 
+    cons_cnt AS (
+      SELECT customer_id, COUNT(*), 
+      	 RANK() OVER(ORDER BY COUNT(*) DESC) rnk 
+      FROM cons_grps
+      GROUP BY customer_id, grp
 )
-SELECT customer_id 
-FROM cons_cnt
-WHERE cons_cnt = (SELECT MAX(cons_cnt) FROM cons_cnt)
+SELECT customer_id
+FROM cons_cnt 
+WHERE rnk = 1 
 ORDER BY customer_id;
