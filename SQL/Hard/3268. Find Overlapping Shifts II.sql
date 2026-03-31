7b31a3550c7d860e9b675e6199c100d26d10fd0b
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-31 11:01:38 +0200

    Update 3268. Find Overlapping Shifts II.sql

--- SQL/Hard/3268. Find Overlapping Shifts II.sql
+++ SQL/Hard/3268. Find Overlapping Shifts II.sql
@@ -3,28 +3,27 @@
 
          -- Approach 1. Using two - CTE -- 
 WITH 
-	delta AS (
-    SELECT employee_id, start_time AS shift_t, 1 AS d FROM employeeshifts
-    UNION ALL
-    SELECT employee_id, end_time AS shift_t, -1 AS d FROM employeeshifts
-  ),
-	grps AS (
-    SELECT employee_id, shift_t,
-        SUM(d) OVER (PARTITION BY employee_id ORDER BY shift_t, d DESC) overlapping_shifts,
-        LEAD(shift_t) OVER (PARTITION BY employee_id ORDER BY shift_t, d DESC) next_shift
-    FROM delta
+	overlap_duration AS (
+      SELECT e1.employee_id, e1.start_time,
+          EXTRACT(EPOCH FROM (e1.end_time - e2.start_time)) / 60 overlap_duration 
+      FROM employeeshifts e1
+      LEFT JOIN employeeShifts e2
+          ON (e1.employee_id = e2.employee_id) 
+          AND (e1.start_time < e2.start_time)
+          AND (e1.end_time BETWEEN e2.start_time AND e2.end_time)
+  	), 
+    overlap_cnt AS (
+      SELECT 
+          employee_id, 
+          COUNT(overlap_duration) + 1 overlap_cnt, 
+          COALESCE(SUM(overlap_duration), 0) total_overlap
+      FROM overlap_duration
+      GROUP BY employee_id, start_time
 )
 SELECT 
-  employee_id,
-  MAX(overlapping_shifts) max_overlapping_shifts,
-  SUM(
-    CASE 
-        WHEN overlapping_shifts > 1 AND next_shift > shift_t 
-        THEN EXTRACT(EPOCH FROM (next_shift - shift_t)) / 60 
-        ELSE 0 
-    END
-  ) total_overlap_duration
-FROM grps
-WHERE next_shift IS NOT NULL
+	employee_id, 
+    MAX(overlap_cnt) max_overlapping_shifts, 
+    SUM(total_overlap) total_overlap_duration
+FROM overlap_cnt
 GROUP BY employee_id
 ORDER BY employee_id;
