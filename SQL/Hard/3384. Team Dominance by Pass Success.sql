267b6c7e29d11bcd54d92120067fdecefe42b167
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-03-31 11:22:31 +0200

    Update 3384. Team Dominance by Pass Success.sql

--- SQL/Hard/3384. Team Dominance by Pass Success.sql
+++ SQL/Hard/3384. Team Dominance by Pass Success.sql
@@ -2,35 +2,30 @@
 
 
          -- Approach 1. Using two - CTE -- 
-WITH 
-	from_to AS (
-      SELECT 
-          t1.team_name from_team, 
-          t2.team_name to_team, 
-          p.time_stamp
-      FROM passes p 
-      JOIN teams t1 ON p.pass_from = t1.player_id 
-      JOIN teams t2 ON p.pass_to = t2.player_id
-	), 
-  calculations AS (
-      SELECT *, 
-        	CASE  
-          		WHEN time_stamp = '45:00' 
-          			OR LEFT(time_stamp, 2)::int < 45 
-          		THEN 1 
-          		ELSE 2 
-        	END half_number, 
-      	  CASE  
-          		WHEN from_team = to_team 
-          		THEN 1 
-          		ELSE -1 
-      	  END passes 
-      FROM from_to 
+WITH calculations AS (
+    SELECT 
+        t1.team_name pass_from, 
+        t2.team_name pass_to, 
+        time_stamp, 
+        CASE 
+            WHEN t1.team_name = t2.team_name 
+            THEN 1 
+            ELSE -1
+        END dominance, 
+        CASE  
+			WHEN time_stamp = '45:00' 
+          		OR LEFT(time_stamp, 2)::int < 45 
+            THEN 1 
+            ELSE 2 
+        END half_number
+    FROM passes p 
+    JOIN teams t1 ON t1.player_id = p.pass_from
+    JOIN teams t2 ON t2.player_id = p.pass_to
 )
 SELECT 
-	  from_team team_name, 
+	pass_from team_name, 
     half_number, 
-    SUM(passes) dominance
-FROM calculations 
-GROUP BY from_team, half_number 
+    SUM(dominance) dominance
+FROM calculations  
+GROUP BY pass_from, half_number
 ORDER BY team_name, half_number;
