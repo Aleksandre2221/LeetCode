a4222d1a02ab612bd60097cb6a6abbb0c06a4c5e
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-04-06 10:36:45 +0200

    Update 3521. Find Product Recommendation Pairs.sql

--- SQL/Medium/3521. Find Product Recommendation Pairs.sql
+++ SQL/Medium/3521. Find Product Recommendation Pairs.sql
@@ -22,3 +22,25 @@ FROM valid_pairs vp
 JOIN productinfo pi1 ON vp.product1_id = pi1.product_id 
 JOIN productinfo pi2 ON vp.product2_id = pi2.product_id
 ORDER BY customer_count DESC, product1_id, product2_id;
+
+
+
+          -- Approach 2. In One Query -- 
+SELECT 
+    p1.product_id product1_id, 
+    p2.product_id product2_id,
+    info1.category product1_category,
+    info2.category product2_category,
+    COUNT(DISTINCT p1.user_id) customer_count
+FROM productpurchases p1  
+JOIN productpurchases p2 
+    ON p1.user_id = p2.user_id
+    AND p1.product_id < p2.product_id
+JOIN productinfo info1 
+    ON info1.product_id = p1.product_id
+JOIN productinfo info2 
+    ON info2.product_id = p2.product_id
+GROUP BY p1.product_id, p2.product_id, info1.category, info2.category 
+HAVING COUNT(DISTINCT p1.user_id) >= 3
+ORDER BY customer_count DESC, product1_id, product2_id;
+
