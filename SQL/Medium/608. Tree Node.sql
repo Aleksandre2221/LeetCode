ea83ad08c527e449b31b46df7bfcc33b3601982f
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-04-12 11:14:15 +0200

    Simplify tree node type classification query
    Refactored SQL query to simplify node type classification.

--- SQL/Medium/608. Tree Node.sql
+++ SQL/Medium/608. Tree Node.sql
@@ -1,31 +1,13 @@
 
 
-         -- Approach 1. My first solution using - CTE / CASE...WHEN / LEFT JOIN -- 
-WITH child_cnt AS (
-  SELECT p_id, COUNT(*) 
-  FROM tree
-  WHERE p_id IS NOT NULL 
-  GROUP BY p_id)
-  
-SELECT tree.id, 
-	CASE  
-    	WHEN tree.p_id IS NULL THEN 'Root'
-        WHEN child_cnt.p_id IS NULL THEN 'Leaf'
-        ELSE 'Inner'
-    END type 
-FROM tree  
-LEFT JOIN child_cnt ON tree.id = child_cnt.p_id;
 
-
-
-
-           -- Approach 2. The BEST one Using only - CASE....WHEN -- 
+           -- Approach 1. The BEST one Using only - CASE....WHEN -- 
 SELECT id, 
     CASE 
         WHEN p_id IS NULL THEN 'Root'
         WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
         ELSE 'Leaf'
     END Type 
-FROM Tree
+FROM Tree;
 
 
