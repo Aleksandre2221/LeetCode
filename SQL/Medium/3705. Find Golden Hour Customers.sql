ada945f0c4df67d85b7062d795253684abb54324
Aleksandre Kobidze <165426119+Aleksandre2221@users.noreply.github.com>
2026-04-16 19:38:55 +0200

    Update 3705. Find Golden Hour Customers.sql

--- SQL/Medium/3705. Find Golden Hour Customers.sql
+++ SQL/Medium/3705. Find Golden Hour Customers.sql
@@ -1,25 +1,26 @@
 
 
-         -- Approach 1. Using - CTE and multiple conditions in - HAVING -- 
-WITH peak_hours AS (
-    SELECT *,
-      CASE 
-          WHEN order_timestamp::time BETWEEN '11:00'::time AND '14:00'::time
-              OR order_timestamp::time BETWEEN '18:00'::time AND '21:00'::time
-          THEN 1
-          ELSE 0
-      END AS is_peak
-    FROM restaurant_orders
-)
-SELECT customer_id,
-  COUNT(*) total_orders,
-  (SUM(is_peak)::numeric * 100 / COUNT(*))::int peak_hour_percentage,
-  ROUND(AVG(order_rating), 2) average_rating
-FROM peak_hours
+         -- Approach 1. Using multiple conditions in - HAVING -- 
+SELECT 
+    customer_id,
+    COUNT(*) total_orders,
+    (SUM(CASE 
+            WHEN order_timestamp::time BETWEEN '11:00'::time AND '14:00'::time 
+              OR order_timestamp::time BETWEEN '18:00'::time AND '21:00'::time 
+            THEN 1.0 
+            ELSE 0 
+         END) * 100 / COUNT(*))::int peak_hour_percentage,
+    ROUND(AVG(order_rating), 2) average_rating
+FROM restaurant_orders
 GROUP BY customer_id
 HAVING 
-  COUNT(*) >= 3
-  AND SUM(is_peak)::numeric / COUNT(*) >= 0.6
-  AND AVG(order_rating) >= 4
-  AND COUNT(order_rating)::numeric / COUNT(*) >= 0.5
+    COUNT(*) >= 3
+    AND SUM(CASE 
+                WHEN order_timestamp::time BETWEEN '11:00'::time AND '14:00'::time 
+                  OR order_timestamp::time BETWEEN '18:00'::time AND '21:00'::time 
+                THEN 1.0 
+                ELSE 0 
+            END) / COUNT(*) >= 0.6
+    AND COUNT(order_rating) * 1.0 / COUNT(*) >= 0.5
+    AND ROUND(AVG(order_rating), 2) >= 4.0
 ORDER BY average_rating DESC, customer_id DESC;
